2fe0800340421ecc3d144899b45c7d3ec6a6342a
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-09-13 10:38:26 +0100

    added stylesheet interface to get-dom-puppeteer module

--- packages/types/get-dom-puppeteer.d.ts
+++ packages/types/get-dom-puppeteer.d.ts
@@ -1,6 +1,7 @@
 declare module '@qualweb/get-dom-puppeteer' {
   import { DomElement } from 'htmlparser2';
-
+  import { Stylesheet } from 'css';
+  
   interface DomOptions {
     mobile?: boolean;
     landscape?: boolean;
@@ -23,9 +24,18 @@ declare module '@qualweb/get-dom-puppeteer' {
     readonly elementCount?: number;
   }
 
+  interface CSSStylesheet {
+    readonly file: string;
+    readonly content?: {
+      plain?: string;
+      parsed?: StyleSheet;
+    };
+  }
+
   interface Dom {
     readonly source: Html;
     readonly processed: Html;
+    readonly stylesheets?: CSSStylesheet[];
   }
 
   function getDom(url: string, options?: DomOptions): Promise<Dom>;
