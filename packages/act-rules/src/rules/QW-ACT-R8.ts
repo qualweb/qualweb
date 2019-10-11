e659220b70458999ee11f4389d7fca367be96ea0
Bruno Andrade <brunocea.1@gmail.com>
2019-10-11 19:34:42 +0100

    all tested, all done

--- packages/act-rules/src/rules/QW-ACT-R8.ts
+++ packages/act-rules/src/rules/QW-ACT-R8.ts
@@ -11,8 +11,7 @@
 'use strict';
 
 import {DomElement, DomUtils} from 'htmlparser2';
-import _ from 'lodash';
-
+import {DomUtils as DomUtil, AccessibilityTreeUtils} from '@qualweb/util';
 import {ACTRule, ACTRuleResult} from '@qualweb/act-rules';
 import Rule from './Rule.object';
 
@@ -60,55 +59,57 @@ class QW_ACT_R8 extends Rule {
     super(rule);
   }
 
-  async execute(element: DomElement | undefined): Promise<void> {
+  async execute(element: DomElement | undefined, processedHTML: DomElement[]): Promise<void> {
     const evaluation: ACTRuleResult = {
       verdict: '',
       description: '',
       resultCode: ''
     };
 
-    let accessName = 'Submit';
-    let isHidden = false;
     let imageFile = new RegExp('(.apng|.bmp|.gif|.ico|.cur|.jpg|.jpeg|.jfif|.pjpeg|.pjp|.png|.svg|.tif|.tiff|.webp)(\\?.+)?$');
 
     if (element === undefined || element.attribs === undefined) { // if the element doesn't exist, there's nothing to test
       evaluation.verdict = 'inapplicable';
       evaluation.description = `There are no HTML elements with semantic role of image`;
       evaluation.resultCode = 'RC1';
-    } else if (isHidden) {
-      evaluation.verdict = 'inapplicable';
-      evaluation.description = `This element is not included in the accessibility tree`;
-      evaluation.resultCode = 'RC2';
-    } else if (element.attribs && element.attribs['role'] && element.attribs['role'] !== 'img') {
-      evaluation.verdict = 'inapplicable';
-      evaluation.description = `This element doesn't have the semantic role of image`;
-      evaluation.resultCode = 'RC3';
     } else {
+      let accessName = AccessibilityTreeUtils.getAccessibleName(element, processedHTML, false);
+      let isHidden = DomUtil.isElementHidden(element);
+      if (isHidden) {
+        evaluation.verdict = 'inapplicable';
+        evaluation.description = `This element is not included in the accessibility tree`;
+        evaluation.resultCode = 'RC2';
+      } else if (element.attribs && element.attribs['role'] && element.attribs['role'] !== 'img') {
+        evaluation.verdict = 'inapplicable';
+        evaluation.description = `This element doesn't have the semantic role of image`;
+        evaluation.resultCode = 'RC3';
+      } else {
 
-      let filepath = element.attribs['src'].split('/');
-      let filenameWithExtension = filepath[filepath.length - 1];
-      let parent = element.parent;
+        let filepath = element.attribs['src'].split('/');
+        let filenameWithExtension = filepath[filepath.length - 1];
+        let parent = element.parent;
 
-      if (filenameWithExtension === accessName) {
-        if (imageFile.test(filenameWithExtension)) {
-          if (parent && parent.name === 'a' && DomUtils.getText(parent)){
-            evaluation.verdict = 'passed';
-            evaluation.description = `This element's accessible name includes the filename but, with the text content of the a element, the image is accurately described`;
-            evaluation.resultCode = 'RC4';
+        if (filenameWithExtension === accessName) {
+          if (imageFile.test(filenameWithExtension)) {
+            if (parent && parent.name === 'a' && DomUtils.getText(parent)){
+              evaluation.verdict = 'passed';
+              evaluation.description = `This element's accessible name includes the filename but, with the text content of the a element, the image is accurately described`;
+              evaluation.resultCode = 'RC4';
+            } else {
+              evaluation.verdict = 'failed';
+              evaluation.description = `The presence of the file extension in the accessible name doesn't accurately describe the image`;
+              evaluation.resultCode = 'RC5';
+            }
           } else {
-            evaluation.verdict = 'failed';
-            evaluation.description = `The presence of the file extension in the accessible name doesn't accurately describe the image`;
-            evaluation.resultCode = 'RC5';
+            evaluation.verdict = 'passed';
+            evaluation.description = `This element's accessible name uses the filename which accurately describes the image`;
+            evaluation.resultCode = 'RC6';
           }
         } else {
-          evaluation.verdict = 'passed';
-          evaluation.description = `This element's accessible name uses the filename which accurately describes the image`;
-          evaluation.resultCode = 'RC6';
-        }
-      } else {
           evaluation.verdict = 'inapplicable';
           evaluation.description = `This element's accessible name is not equivalent to the file name specified in the src attribute.`;
           evaluation.resultCode = 'RC7';
+        }
       }
     }
 
