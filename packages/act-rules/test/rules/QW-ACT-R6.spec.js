4683178d9ba470b5561d69c2bcf501e2de5262cf
Estriga <fc47839@alunos.fc.ul.pt>
2019-10-16 18:26:14 +0100

    done

--- packages/act-rules/test/rules/QW-ACT-R6.spec.js
+++ packages/act-rules/test/rules/QW-ACT-R6.spec.js
@@ -10,7 +10,7 @@ const { expect } = require('chai');
 describe('Rule QW-ACT-R6', function () {
 
   const tests = [
-    /**{
+    {
       url: 'https://act-rules.github.io/testcases/59796f/2400cdac870a6244889b8fccea54425320e8749f.html',
       outcome: 'passed'
     },
@@ -21,11 +21,11 @@ describe('Rule QW-ACT-R6', function () {
     {
       url: 'https://act-rules.github.io/testcases/59796f/48e5a8f7cb212b2f7605487e139f262545caca89.html',
       outcome: 'passed'
-    },*/
+    },
     {
       url: 'https://act-rules.github.io/testcases/59796f/006dd89a7eeebee98ec012a2666a03844e64b01a.html',
       outcome: 'passed'
-    }/**,
+    },
     {
       url: 'https://act-rules.github.io/testcases/59796f/4d797a9fddf365ae209130e381de006becda625e.html',
       outcome: 'passed'
@@ -41,11 +41,11 @@ describe('Rule QW-ACT-R6', function () {
     {
       url: 'https://act-rules.github.io/testcases/59796f/366185c2831d044c6a9036b1cf76eb13c81d2816.html',
       outcome: 'failed'
-    }*/,
+    },
     {
       url: 'https://act-rules.github.io/testcases/59796f/ba6e135ea5b6b57b49e024d3bbb5fcfa57a54fc5.html',
       outcome: 'failed'
-    }/**,
+    },
     {
       url: 'https://act-rules.github.io/testcases/59796f/d40c0add6e960291598091cc969d69efc29038a1.html',
       outcome: 'failed'
@@ -69,7 +69,7 @@ describe('Rule QW-ACT-R6', function () {
     {
       url: 'https://act-rules.github.io/testcases/59796f/79adbfb491069d70cc39446655c1815c7b765d6a.html',
       outcome: 'inapplicable'
-    }*/
+    }
   ];
 
   let i = 0;
