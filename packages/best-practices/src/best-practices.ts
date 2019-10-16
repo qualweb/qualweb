c657a874cdc741119a3d7b877e767283152bb80e
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-10-16 16:42:57 +0100

    QW-BP6 done. Needs reviews and tests

--- packages/best-practices/src/best-practices.ts
+++ packages/best-practices/src/best-practices.ts
@@ -1,9 +1,11 @@
 'use strict';
 
 import QW_BP1 from './best-practices/QW-BP1';
+import QW_BP6 from './best-practices/QW-BP6';
 
 const bestPractices = {
-  'QW-BP1': new QW_BP1()
+  'QW-BP1': new QW_BP1(),
+  'QW-BP6': new QW_BP6()
 };
 
 export {
---
+++ packages/best-practices/test/best-practices/QW-BP6.spec.js
@@ -0,0 +1,7 @@
+const { executeBestPractices } = require('../../dist/index');
+const { expect } = require('chai');
+const { getDom } = require('@qualweb/get-dom-puppeteer');
+
+describe('Best practice QW-BP1', function () {
+  //TODO: best practice QW-BP6 unit tests
+});
