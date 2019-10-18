a802aa852e273709e0257d57606b48fa046963e4
João Afonso Vicente <joao.vicente@campus.ul.pt>
2019-10-18 18:06:31 +0100

    QW-BP10 done. Needs reviews and tests

--- packages/best-practices/src/best-practices.ts
+++ packages/best-practices/src/best-practices.ts
@@ -1,9 +1,11 @@
 'use strict';
 
 import QW_BP1 from './best-practices/QW-BP1';
+import QW_BP10 from './best-practices/QW-BP10';
 
 const bestPractices = {
-  'QW-BP1': new QW_BP1()
+  'QW-BP1': new QW_BP1(),
+  'QW-BP10': new QW_BP10()
 };
 
 export {
