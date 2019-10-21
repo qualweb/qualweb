642c1e78c272a3b82ac082419bd751d8fe842ac8
Estriga <fc47839@alunos.fc.ul.pt>
2019-10-21 17:42:05 +0100

    fixed test

--- packages/act-rules/test/rules/QW-ACT-R14.spec.js
+++ packages/act-rules/test/rules/QW-ACT-R14.spec.js
@@ -1,41 +1,42 @@
 const {
     configure,
     executeACTR,
-} = require('../../dist/index');
-const {
+    resetConfiguration
+  } = require('../../dist/index');
+  const {
     getDom
-} = require('@qualweb/get-dom-puppeteer');
-const { expect } = require('chai');
-const request = require('request-promise');
-
-describe('Rule QW-ACT-R14', function () {
-
-  const ruleId = 'b4f0c3';
-  let testCases = [];
-
-  it("", async function () {
-    const json = JSON.parse(await request('https://act-rules.github.io/testcases.json'));
-    testCases = json.testcases.filter(tc => tc.ruleId === ruleId);
-
-    let i = 0;
-    let lastOutcome = 'passed';
-    for (const test of testCases || []) {
-      if (test.expected !== lastOutcome) {
-        lastOutcome = test.expected;
-        i = 0;
-      }
-      i++;
-      describe(`${test.expected.charAt(0).toUpperCase() + test.expected.slice(1)} example ${i}`, function () {
-        it(`should have outcome="${test.expected}"`, async function () {
-          this.timeout(10 * 1000);
-          const {source, processed, stylesheets} = await getDom(test.url);
-          configure({
-            rules: ['QW-ACT-R14']
+  } = require('@qualweb/get-dom-puppeteer');
+  const { expect } = require('chai');
+  const request = require('request-promise');
+  
+   describe('Rule QW-ACT-R1', async function () {
+  
+  
+    const ruleId = 'b4f0c3';
+    let testCases = [];
+  
+      it("", async function () {
+      const json = JSON.parse(await request('https://act-rules.github.io/testcases.json'));
+      testCases = json.testcases.filter(tc => tc.ruleId === ruleId);
+     // console.log(testCases)
+      let i = 0;
+      let lastOutcome = 'passed';
+      for (const test of testCases || []) {
+        if (test.expected !== lastOutcome) {
+          lastOutcome = test.expected;
+          i = 0;
+        }
+        i++;
+        describe(`${test.expected.charAt(0).toUpperCase() + test.expected.slice(1)} example ${i}`, function () {
+          it(`should have outcome="${test.expected}"`, async function () {
+            this.timeout(10 * 1000);
+            const { source, processed, stylesheets } = await getDom(test.url);
+            configure({
+              rules: ['QW-ACT-R14']
+            });
+            const report = await executeACTR(test.url, source.html.parsed, processed.html.parsed, stylesheets);
+            expect(report.rules['QW-ACT-R14'].metadata.outcome).to.be.equal(test.expected);
           });
-          const report = await executeACTR(test.url, source.html.parsed, processed.html.parsed, stylesheets);
-          expect(report.rules['QW-ACT-14'].metadata.outcome).to.be.equal(test.expected);
         });
-      });
-    }
+      }});
   });
-});
