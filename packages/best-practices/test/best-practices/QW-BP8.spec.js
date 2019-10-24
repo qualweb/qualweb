d24f9ebd25cb71df4dac38c9b2f968befe77b7dc
Bruno Andrade <brunocea.1@gmail.com>
2019-10-24 12:34:27 +0100

    add test

--- packages/best-practices/test/best-practices/QW-BP8.spec.js
+++ packages/best-practices/test/best-practices/QW-BP8.spec.js
@@ -2,6 +2,34 @@ const { executeBestPractices } = require('../../dist/index');
 const { expect } = require('chai');
 const { getDom } = require('@qualweb/get-dom-puppeteer');
 
-describe('Best practice QW-BP8', function () {
-  //TODO: best practice QW-BP8 unit tests
+describe('Best Practice QW-BP8', function () {
+  const tests = [
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/h42/warning1.html',
+      outcome: 'warning'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~bandrade/h42/failed1.html',
+      outcome: 'failed'
+    }
+  ];
+
+  let i = 0;
+  let lastOutcome = 'warning';
+  for (const test of tests || []) {
+    if (test.outcome !== lastOutcome) {
+      lastOutcome = test.outcome;
+      i = 0;
+    }
+    i++;
+    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
+      it(`should have outcome="${test.outcome}"`, async function () {
+        this.timeout(10 * 1000);
+        const { source, processed } = await getDom(test.url);
+
+        const report = await executeBestPractices(source.html.parsed, processed.html.parsed);
+        expect(report['best-practices']['QW-BP8'].metadata.outcome).to.be.equal(test.outcome);
+      });
+    });
+  }
 });
