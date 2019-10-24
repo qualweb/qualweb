01e4074da4bf5216a848bd055d077f20072ebe5a
aestriga <aestriga@lasige.di.fc.ul.pt>
2019-10-24 16:18:45 +0100

    fixed name from content

--- packages/util/src/accessibilityTreeUtils/allowsNameFromContent.ts
+++ packages/util/src/accessibilityTreeUtils/allowsNameFromContent.ts
@@ -7,7 +7,8 @@ function allowsNameFromContent(element: DomElement): boolean {
 
   let role,name;
   name = element.name;
-  let nameFromContentElements = ["button","h1","h2","h3","h4","h5","h6","a","link","listitem","option","menuitem","option","tr","text"];
+  let nameFromContentElements = ["button","h1","h2","h3","h4","h5","h6","a","link","option","output", "summary", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "dfn", "em", "i", "kbd"
+    , "mark", "q", "rp", "rt", "ruby", "s", "samp", "small", "strong", "sub", "and", "sup", "time", "u", "var", "wbr"];
 
   if (element.attribs !== undefined)
       role = element.attribs["role"];
