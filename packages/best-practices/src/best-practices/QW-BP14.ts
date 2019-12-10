4a5ced7a806fee704a3eec7af5ae685dace84932
arbsantos <andre13521@gmail.com>
2019-12-10 13:51:24 +0000

    bp14 and tests

---
+++ packages/best-practices/src/best-practices/QW-BP14.ts
@@ -0,0 +1,110 @@
+'use strict';
+
+import {
+  BestPractice as BestPracticeType,
+  BestPracticeResult
+} from '@qualweb/best-practices';
+import {
+  ElementHandle,
+  Page
+} from 'puppeteer';
+// import { DomUtils } from '@qualweb/util';
+import {
+  CSSStylesheet
+} from '@qualweb/core';
+import BestPractice from './BestPractice.object';
+
+const bestPractice: BestPracticeType = {
+  name: `At least one container's width has been specified using values expressed in px`,
+  code: 'QW-BP13',
+  description: `At least one container's width has been specified using values expressed in px`,
+  metadata: {
+    target: {
+      element: 'span, article, section, nav, aside, hgroup, header, footer, address, p, hr, blockquote, div, h1, h2, h3, h4, h5, h6, li, ul, ol, dd, dt, dl, figcaption'
+    },
+    passed: 0,
+    warning: 0,
+    failed: 0,
+    inapplicable: 0,
+    outcome: '',
+    description: ''
+  },
+  results: new Array < BestPracticeResult > ()
+};
+
+class QW_BP14 extends BestPractice {
+
+  containers = ["span", "article", "section", "nav", "aside", "hgroup", "header", "footer", "address", "p", "hr", "blockquote", "div", "h1", "h2", "h3", "h4", "h5", "h6", "li", "ul", "ol", "dd", "dt", "dl", "figcaption"]
+
+  constructor() {
+    super(bestPractice);
+  }
+
+  async execute(element: ElementHandle | undefined, page: Page | undefined, styleSheets: CSSStylesheet[] | undefined): Promise < void > {
+
+    if (!styleSheets) {
+      return;
+    }
+
+    const evaluation: BestPracticeResult = {
+      verdict: '',
+      description: '',
+      resultCode: ''
+    };
+
+    for (const styleSheet of styleSheets) {
+      if (styleSheet.content && styleSheet.content.plain) {
+        this.analyseAST(styleSheet.content.parsed, styleSheet.file, evaluation);
+      }
+    }
+
+    super.addEvaluationResult(evaluation);
+  }
+  private analyseAST(cssObject: any, fileName: string, evaluation: BestPracticeResult): void {
+    if (cssObject === undefined ||
+      cssObject['type'] === 'comment' ||
+      cssObject['type'] === 'keyframes' ||
+      cssObject['type'] === 'import') { // ignore
+      return;
+    }
+    if (cssObject['type'] === 'rule' || cssObject['type'] === 'font-face' || cssObject['type'] === 'page') {
+      this.loopDeclarations(cssObject, fileName, evaluation)
+    } else {
+      if (cssObject['type'] === 'stylesheet') {
+        for (const key of cssObject['stylesheet']['rules']) {
+          this.analyseAST(key, fileName, evaluation);
+        }
+      } else {
+        for (const key of cssObject['rules']) {
+          this.analyseAST(key, fileName, evaluation);
+        }
+      }
+    }
+  }
+  private loopDeclarations(cssObject: any, fileName: string, evaluation: BestPracticeResult): void {
+
+    let declarations = cssObject['declarations'];
+    if (declarations && this.containers.includes(cssObject["selectors"][0])) {
+      for (const declaration of declarations) {
+        if (declaration['property'] && declaration['value']) {
+          if (declaration['property'] === 'width') {
+            this.extractInfo(cssObject, declaration, fileName, evaluation);
+          }
+        }
+      }
+    }
+  }
+  private extractInfo(cssObject: any, declaration: any, fileName: string, evaluation: BestPracticeResult): void {
+    if (declaration['value'].endsWith("px")) {
+      evaluation.verdict = 'failed';
+      evaluation.description = `At least one container's width has been specified using values expressed in px`;
+      evaluation.resultCode = 'RC1';
+    } else {
+      evaluation.verdict = 'passed';
+      evaluation.description = `At least one container's width has been specified using values expressed other metric than px`;
+      evaluation.resultCode = 'RC2';
+    }
+  }
+}
+
+export = QW_BP14;
---
+++ packages/best-practices/test/best-practices/QW-BP14.spec.js
@@ -0,0 +1,41 @@
+const { executeBestPractices } = require('../../dist/index');
+const { expect } = require('chai');
+const puppeteer = require('puppeteer');
+const {
+  getDom
+} = require('../getDom');
+describe('Technique QW-BP14', function () {
+  const tests = [
+    {
+      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/BP14/test1.html',
+      outcome: 'failed'
+    }
+  ];
+  let browser;
+  it("pup open", async function () {
+    browser = await puppeteer.launch();
+  });
+  let i = 0;
+  let lastOutcome = 'warning';
+  for (const test of tests || []) {
+    if (test.outcome !== lastOutcome) {
+      lastOutcome = test.outcome;
+      i = 0;
+    }
+    i++;
+    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
+      it(`should have outcome="${test.outcome}"`, async function () {
+        this.timeout(10 * 1000);
+        const { stylesheets } = await getDom(browser,test.url);
+
+        const report = await executeBestPractices(undefined, stylesheets);
+        expect(report['best-practices']['QW-BP14'].metadata.outcome).to.be.equal(test.outcome);
+      });
+    });
+  }
+  describe(``,  function () {
+    it(`pup shutdown`, async function () {
+      await browser.close();
+    });
+  });
+});
