5ef5bfd797eb3751db86392bfc51d50edfe07ee3
João Vicente <joao.vicente@campus.ul.pt>
2019-12-11 15:45:39 +0000

    removed getLabet function

--- packages/util/src/accessibilityTreeUtils/accessibilityTreeUtils.ts
+++ packages/util/src/accessibilityTreeUtils/accessibilityTreeUtils.ts
@@ -5,7 +5,6 @@ import elementHasRoleNoneOrPresentationFunction from "./elementHasRoleNoneOrPres
 import getAccessibleNameFunction from "./getAccessibleName";
 import getDefaultNameFunction from "./getDefaultName";
 import getAccessibleNameSVGFunction from "./getAccessibleNameSVG";
-import getLabelFunction from "./getLabel";
 import getTrimmedTextFunction from "./getTrimmedText";
 import isDataTableFunction from "./isDataTable";
 import isElementChildOfDetailsFunction from "./isElementChildOfDetails";
@@ -20,7 +19,6 @@ namespace AccessibilityTreeUtils {
   export const getAccessibleName = getAccessibleNameFunction;
   export const getDefaultName = getDefaultNameFunction;
   export const getAccessibleNameSVG = getAccessibleNameSVGFunction;
-  export const getLabel = getLabelFunction;
   export const getTrimmedText = getTrimmedTextFunction;
   export const isDataTable = isDataTableFunction;
   export const isElementChildOfDetails = isElementChildOfDetailsFunction;
