359e43f388b2e7f8d8ced643acfaabbd857752a9
João Vicente <joao.vicente@campus.ul.pt>
2019-12-12 16:49:29 +0000

    fixed getElementChildTextContent function

--- packages/util/src/domUtils/getElementChildTextContent.ts
+++ packages/util/src/domUtils/getElementChildTextContent.ts
@@ -4,17 +4,18 @@ import {
   ElementHandle
 } from 'puppeteer';
 
-async function getElementChildTextContent(element: ElementHandle, childName: string): Promise<string | undefined> {
+function getElementChildTextContent(element: ElementHandle, childName: string): Promise<string | undefined> {
   if (!element) {
     throw Error('Element is not defined');
   }
 
   return element.evaluate((elem, childName) => {
-    for (const child of elem.children) {
+    for (const child of elem.children || []) {
       if (child.tagName.toLowerCase() === childName.toLowerCase()) {
-        return child['text'];
+        return child['textContent'] || undefined;
       }
     }
+    return undefined;
   }, childName);
 }
 
