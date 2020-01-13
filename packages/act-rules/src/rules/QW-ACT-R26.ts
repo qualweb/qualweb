db7c1ac6867d03ba1f461dd3f9a0100203836956
João Vicente <joao.vicente@campus.ul.pt>
2020-01-13 18:26:05 +0000

    updated rule

--- packages/act-rules/src/rules/QW-ACT-R26.ts
+++ packages/act-rules/src/rules/QW-ACT-R26.ts
@@ -2,48 +2,49 @@
 
 import { ElementHandle, Page } from 'puppeteer';
 import Rule from './Rule.object';
-import { ACTRule, ACTRuleResult } from '@qualweb/act-rules';
+import { ACTRuleResult } from '@qualweb/act-rules';
 import { DomUtils } from '@qualweb/util';
-
-const rule: ACTRule = {
-  name: 'video element auditory content has accessible alternative',
-  code: 'QW-ACT-R26',
-  mapping: 'eac66b',
-  description: 'This rule checks that video elements have an alternative for information conveyed through audio.',
-  metadata: {
-    target: {
-      element: 'video'
-    },
-    'success-criteria': [
-      {
-        name: '1.2.2',
-        level: 'A',
-        principle: 'Perceivable ',
-        url: 'https://www.w3.org/WAI/WCAG21/Understanding/captions-prerecorded.html'
-      }
-    ],
-    related: [],
-    url: 'https://act-rules.github.io/rules/eac66b',
-    passed: 0,
-    warning: 0,
-    inapplicable: 0,
-    failed: 0,
-    type: ['ACTRule', 'TestCase'],
-    a11yReq: ['WCAG21:language'],
-    outcome: '',
-    description: ''
-  },
-  results: new Array<ACTRuleResult>()
-};
 
 class QW_ACT_R26 extends Rule {
 
   constructor() {
-    super(rule);
+    super({
+      name: 'video element auditory content has accessible alternative',
+      code: 'QW-ACT-R26',
+      mapping: 'eac66b',
+      description: 'This rule checks that video elements have an alternative for information conveyed through audio.',
+      metadata: {
+        target: {
+          element: 'video'
+        },
+        'success-criteria': [
+          {
+            name: '1.2.2',
+            level: 'A',
+            principle: 'Perceivable ',
+            url: 'https://www.w3.org/WAI/WCAG21/Understanding/captions-prerecorded.html'
+          }
+        ],
+        related: [],
+        url: 'https://act-rules.github.io/rules/eac66b',
+        passed: 0,
+        warning: 0,
+        inapplicable: 0,
+        failed: 0,
+        type: ['ACTRule', 'TestCase'],
+        a11yReq: ['WCAG21:language'],
+        outcome: '',
+        description: ''
+      },
+      results: new Array<ACTRuleResult>()
+    });
   }
 
   async execute(element: ElementHandle | undefined, page: Page): Promise<void> {
 
+    if (!element) {
+      return;
+    }
 
     const evaluation: ACTRuleResult = {
       verdict: '',
@@ -51,48 +52,47 @@ class QW_ACT_R26 extends Rule {
       resultCode: ''
     };
 
-    if (element === undefined) {
-      evaluation.verdict = 'inapplicable';
-      evaluation.description = "No video element";
+    const [track, isVisible, metadata] = await Promise.all([
+      element.$('track[kind="captions"]'),
+      DomUtils.isElemenVisible(element),
+      DomUtils.getVideoMetadata(element)
+    ]);
+
+    const hasPupeteerApplicableData = metadata.puppeteer.video.duration > 0 && metadata.puppeteer.audio.hasSoundTrack;
+    const applicableServiceData = metadata.service.video.duration > 0 && metadata.service.audio.duration > 0 && metadata.service.audio.volume !== -91;
+
+    if (metadata.service.error && metadata.puppeteer.error) {
+      evaluation.verdict = 'warning';
+      evaluation.description = "Cant colect data from the video element";
       evaluation.resultCode = 'RC1';
-    } else {
-      let track = await element.$('track[kind="captions"]')
-      let isVisible = await DomUtils.isElemenVisible(element);
-      let metadata = await DomUtils.getVideoMetadata(element);
-      let hasPupeteerApplicableData = metadata.puppeteer.video.duration > 0 && metadata.puppeteer.audio.hasSoundTrack;
-      let applicableServiceData = metadata.service.video.duration > 0 && metadata.service.audio.duration > 0 && metadata.service.audio.volume !== -91;
-
-      if (metadata.service.error && metadata.puppeteer.error) {
+    } else if (isVisible && applicableServiceData) {
+      if (track !== null) {
         evaluation.verdict = 'warning';
-        evaluation.description = "Cant colect data from the video element";
+        evaluation.description = "Check if the track element correctly describes the auditive content of the video";
         evaluation.resultCode = 'RC1';
-      } else if (isVisible && applicableServiceData) {
-
-        if (track !== null) {
-          evaluation.verdict = 'warning';
-          evaluation.description = "Check if the track element correctly describes the auditive content of the video";
-          evaluation.resultCode = 'RC1';
-        }
-        else {
-          evaluation.verdict = 'warning';
-          evaluation.description = "Check if the video element auditive content has accessible alternative";
-          evaluation.resultCode = 'RC2';
-        }
-      } else if (isVisible && hasPupeteerApplicableData) {
-        evaluation.verdict = 'warning';
-        evaluation.description = "Video has a sound track but we can verify the volume.Check if the video has audio and if it does check if the video element auditive content has an accessible alternative";
-        evaluation.resultCode = 'RC3';
-
       } else {
-        evaluation.verdict = 'inapplicable';
-        evaluation.description = "The video element isn't a non-streaming video element that is visible, where the video contains audio.";
-        evaluation.resultCode = 'RC4';
+        evaluation.verdict = 'warning';
+        evaluation.description = "Check if the video element auditive content has accessible alternative";
+        evaluation.resultCode = 'RC2';
       }
+    } else if (isVisible && hasPupeteerApplicableData) {
+      evaluation.verdict = 'warning';
+      evaluation.description = "Video has a sound track but we can verify the volume.Check if the video has audio and if it does check if the video element auditive content has an accessible alternative";
+      evaluation.resultCode = 'RC3';
+    } else {
+      evaluation.verdict = 'inapplicable';
+      evaluation.description = "The video element isn't a non-streaming video element that is visible, where the video contains audio.";
+      evaluation.resultCode = 'RC4';
     }
 
-    if (element !== undefined) {
-      evaluation.htmlCode = await DomUtils.getElementHtmlCode(element);
-      evaluation.pointer = await DomUtils.getElementSelector(element);
+    if (element) {
+      const [htmlCode, pointer] = await Promise.all([
+        DomUtils.getElementHtmlCode(element),
+        DomUtils.getElementSelector(element)
+      ]);
+      
+      evaluation.htmlCode = htmlCode;
+      evaluation.pointer = pointer;
     }
     super.addEvaluationResult(evaluation);
   }
--- packages/act-rules/test/rules/QW-ACT-R1.spec.js
+++ packages/act-rules/test/rules/QW-ACT-R1.spec.js
@@ -1,225 +1,40 @@
-const {
-  configure,
-  executeACTR,
-  resetConfiguration
-} = require('../../dist/index');
-const {
-  getDom
-} = require('@qualweb/get-dom-puppeteer');
 const { expect } = require('chai');
-const request = require('request-promise');
+const puppeteer = require('puppeteer');
+const path = require('path');
 
-describe('Rule QW-ACT-R1', function () {
+const { mapping } = require('../constants');
+const { getTestCases, getDom } = require('../getDom');
+const { configure, executeACTR } = require('../../dist/index');
+
+const rule = path.basename(__filename).split('.')[0];
+const ruleId = mapping[rule];
+
+describe(`Rule ${rule}`, async function () {
   
-  const URL = 'http://accessible-serv.lasige.di.fc.ul.pt/~jvicente/test/';
-
-  describe('Configuration', function () {
-    describe('The rule should...', function() {
-      it('...be executed without calling the "configure" function', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
-      it('...be executed when using rules=["QW-ACT-R1"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          rules: ['QW-ACT-R1']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
-      it('...be executed when using rules=["2779a5"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          rules: ['2779a5']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Operable"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Operable']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
-      it('...be executed when using levels=["A"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          levels: ['A']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
-      it('...be executed when using principles=["Operable"] and levels=["A"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Operable'],
-          levels: ['A']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Perceivable", "Understandable", "Robust"] and levels=["AA", "AAA"] and rules=["QW-ACT-R1"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Robust', 'Understandable', 'Perceivable'],
-          levels: ['AA', 'AAA'],
-          rules: ['QW-ACT-R1']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.include('QW-ACT-R1');
-      });
+  it('Starting testbench', async function () {
+    const browser = await puppeteer.launch();
+    const data = JSON.parse(await getTestCases());
+    const tests = data.testcases.filter(t => t.ruleId === ruleId).map(t => {
+      return { title: t.testcaseTitle, url: t.url, outcome: t.expected };
     });
 
-    describe('The rule should not...', function() {
-      it('...be executed when using other than rules=["QW-ACT-R1"] or rules=["2779a5"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          rules: ['QW-ACT-R2']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Perceivable"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Perceivable']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Understandable"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Understandable']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Robust"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Robust']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using levels=["AA"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          levels: ['AA']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using levels=["AAA"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          levels: ['AAA']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Operable"] and levels=["AA"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Operable'],
-          levels: ['AA']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Operable"] and levels=["AAA"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Operable'],
-          levels: ['AAA']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Perceivable"] and levels=["A"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Perceivable'],
-          levels: ['A']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Understandable"] and levels=["A"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Understandable'],
-          levels: ['A']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-      it('...be executed when using principle=["Robust"] and levels=["A"]', async function () {
-        this.timeout(10 * 1000);
-        const { source, processed, stylesheets } = await getDom(URL);
-        configure({
-          principles: ['Robust'],
-          levels: ['A']
-        });
-        const report = await executeACTR(URL, source.html.parsed, processed.html.parsed, stylesheets);
-        expect(Object.keys(report.rules)).to.be.an('array').and.to.not.include('QW-ACT-R1');
-      });
-    });
-  });
+    describe('Running tests', function() {
+      for (const test of tests || []) {
+        it(test.title, async function() {
+          this.timeout(100 * 1000);
+          const { sourceHtml, page, stylesheets } = await getDom(browser, test.url);
+          configure({ rules: [rule] });
+          const report = await executeACTR(sourceHtml, page, stylesheets);
 
-  afterEach(function() {
-    resetConfiguration();
-  });
-
-  const ruleId = '2779a5';
-  let testCases = [];
-
-  before('Downloading tests cases', async function() {
-    const json = JSON.parse(await request('https://act-rules.github.io/testcases.json'));
-    testCases = json.testcases.filter(tc => tc.ruleId === ruleId);
-  });
-
-  setTimeout(function() {
-    let i = 0;
-    let lastOutcome = 'passed';
-    for (const test of testCases || []) {
-      if (test.expected !== lastOutcome) {
-        lastOutcome = test.expected;
-        i = 0;
+          expect(report.rules[rule].metadata.outcome).to.be.equal(test.outcome);
+        });
       }
-      i++;
-      describe(`${test.expected.charAt(0).toUpperCase() + test.expected.slice(1)} example ${i}`, function () {
-        it(`should have outcome="${test.expected}"`, async function () {
-          this.timeout(10 * 1000);
-          const { source, processed, stylesheets } = await getDom(test.url);
-          configure({
-            rules: ['QW-ACT-R1']
-          });
-          const report = await executeACTR(test.url, source.html.parsed, processed.html.parsed, stylesheets);
-          expect(report.rules['QW-ACT-R1'].metadata.outcome).to.be.equal(test.expected);
-        });
+    });
+
+    describe(`Closing testbench`, async function () {
+      it(`Closed`, async function () {
+        await browser.close();
       });
-    }
-  }, 5000);
+    });
+  });
 });
