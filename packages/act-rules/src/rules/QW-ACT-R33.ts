37963a6522d88687118599bdabd76635f89fd27b
João Vicente <joao.vicente@campus.ul.pt>
2020-01-14 11:54:06 +0000

    updated rule

--- packages/act-rules/src/rules/QW-ACT-R33.ts
+++ packages/act-rules/src/rules/QW-ACT-R33.ts
@@ -21,7 +21,8 @@ class QW_ACT_R33 extends Rule {
       description: 'This rule checks that an element with an explicit semantic role exists inside its required context.',
       metadata: {
         target: {
-          element: '*'
+          element: '*',
+          attributes: 'role'
         },
         'success-criteria': [{
           name: '1.3.1',
@@ -56,19 +57,20 @@ class QW_ACT_R33 extends Rule {
       resultCode: ''
     };
 
-    let explictiRole = await DomUtils.getElementAttribute(element, "role");
-    console.log(explictiRole)
-    let implicitRole = await AccessibilityTreeUtils.getImplicitRole(element, page);
-    console.log(implicitRole);
-    let isInAT = await AccessibilityTreeUtils.isElementInAT(element, page);
-    let isValidRole = await AccessibilityTreeUtils.elementHasValidRole(element, page);
+    const [explictiRole, implicitRole, isInAT, isValidRole] = await Promise.all([
+      DomUtils.getElementAttribute(element, "role"),
+      AccessibilityTreeUtils.getImplicitRole(element, page),
+      AccessibilityTreeUtils.isElementInAT(element, page),
+      AccessibilityTreeUtils.elementHasValidRole(element, page)
+    ]);
 
     if (explictiRole !== null && isValidRole && explictiRole !== implicitRole && isInAT && rolesJSON[explictiRole]["requiredContextRole"] !== "") {
-      let requiredContextRole = rolesJSON[explictiRole]["requiredContextRole"];
-      let id = await DomUtils.getElementAttribute(element, "id");
-      let ariaOwns = await page.$('[aria-owns' + `="${id}"]`);
+      const requiredContextRole = rolesJSON[explictiRole]["requiredContextRole"];
+      const id = await DomUtils.getElementAttribute(element, "id");
+      const ariaOwns = await page.$('[aria-owns' + `="${id}"]`);
+
       if (ariaOwns !== null) {
-        let ariaOwnsRole = await AccessibilityTreeUtils.getElementRole(ariaOwns, page);
+        const ariaOwnsRole = await AccessibilityTreeUtils.getElementRole(ariaOwns, page);
         if (requiredContextRole.includes(ariaOwnsRole)) {
           evaluation.verdict = 'passed';
           evaluation.description = `Parent has required context role.`;
@@ -78,7 +80,6 @@ class QW_ACT_R33 extends Rule {
           evaluation.description = `Parent doesnt have required context role`;
           evaluation.resultCode = 'RC2';
         }
-
       } else if (await this.isElementADescendantOf(element, page, requiredContextRole)) {
         evaluation.verdict = 'passed';
         evaluation.description = `Parent has required context role.`;
@@ -88,16 +89,17 @@ class QW_ACT_R33 extends Rule {
         evaluation.description = `Parent doesnt have required context role`;
         evaluation.resultCode = 'RC2';
       }
-
-
-
     } else {
       evaluation.verdict = 'inapplicable';
       evaluation.description = `Element is not in Acessiblity Tree or doesnt have an explicit role with required context role`;
       evaluation.resultCode = 'RC3';
     }
-    evaluation.htmlCode = await DomUtils.getElementHtmlCode(element);
-    evaluation.pointer = await DomUtils.getElementSelector(element);
+    const [htmlCode, pointer] = await Promise.all([
+      DomUtils.getElementHtmlCode(element),
+      DomUtils.getElementSelector(element)
+    ]);
+    evaluation.htmlCode = htmlCode;
+    evaluation.pointer = pointer;
 
     super.addEvaluationResult(evaluation);
   }
@@ -106,12 +108,12 @@ class QW_ACT_R33 extends Rule {
     if (!element) {
       throw Error('Element is not defined');
     }
-    let parent = await DomUtils.getElementParent(element);
+    const parent = await DomUtils.getElementParent(element);
     let result = false;
     let sameRole;
 
     if (parent !== null) {
-      let parentRole = await AccessibilityTreeUtils.getElementRole(parent, page);
+      const parentRole = await AccessibilityTreeUtils.getElementRole(parent, page);
       if (parentRole !== null) {
         sameRole = roles.includes(parentRole);
       }
