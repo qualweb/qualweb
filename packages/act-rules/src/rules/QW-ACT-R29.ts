4072585838269ff175914ae484ed98cd7902d8a2
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-01-14 18:16:54 +0000

    almost done

---
+++ packages/act-rules/src/rules/QW-ACT-R29.ts
@@ -0,0 +1,98 @@
+'use strict';
+
+import { ElementHandle, Page } from 'puppeteer';
+import Rule from './Rule.object';
+import { ACTRuleResult } from '@qualweb/act-rules';
+import { DomUtils, AccessibilityTreeUtils } from '@qualweb/util';
+
+class QW_ACT_R29 extends Rule {
+
+  constructor() {
+    super({
+      name: 'Audio element content has text alternative',
+      code: 'QW-ACT-R29',
+      mapping: 'e7aa44',
+      description: 'This rule checks if audio only elements have a text alternative available.',
+      metadata: {
+        target: {
+          element: '*',
+          attributes: 'role'
+        },
+        'success-criteria': [
+          {
+            name: '1.2.1',
+            level: 'A',
+            principle: 'Perceivable ',
+            url: 'https://www.w3.org/WAI/WCAG21/Understanding/audio-only-and-video-only-prerecorded'
+          }
+        ],
+        related: [],
+        url: 'https://act-rules.github.io/rules/e7aa44',
+        passed: 0,
+        warning: 0,
+        inapplicable: 0,
+        failed: 0,
+        type: ['ACTRule', 'TestCase'],
+        a11yReq: ['WCAG21:language'],
+        outcome: '',
+        description: ''
+      },
+      results: new Array<ACTRuleResult>()
+    });
+  }
+
+  async execute(element: ElementHandle | undefined, page: Page): Promise<void> {
+
+
+    let evaluation: ACTRuleResult = {
+      verdict: '',
+      description: '',
+      resultCode: ''
+    };
+    if (element === undefined) {
+      evaluation.verdict = 'inapplicable';
+      evaluation.description = "No audio element";
+      evaluation.resultCode = 'RC1';
+      super.addEvaluationResult(evaluation);
+    } else {
+
+      let [isInAT, isVisible, controls, autoPlay, metadata] = await Promise.all([
+        AccessibilityTreeUtils.isElementInAT(element, page),
+        DomUtils.isElemenVisible(element, page),
+        DomUtils.elementHasAttribute(element, "controls"),
+        DomUtils.getElementAttribute(element, "autoplay"),
+        DomUtils.getVideoMetadata(element)
+      ]);
+      let duration = metadata["puppeteer"]["video"]["duration"];
+
+      console.log(metadata);
+      console.log(duration)
+      console.log(autoPlay)
+      console.log(isVisible)
+      console.log(isInAT)
+      console.log(controls);
+
+      if (duration > 0 && (isInAT && isVisible && controls || autoPlay)) {
+        evaluation.verdict = 'warning';
+        evaluation.description = "Check if audio has text-alternative";
+        evaluation.resultCode = 'RC2';
+      } else {
+        evaluation.verdict = 'inapplicable';
+        evaluation.description = "Element is not a non-streaming audio element with autoplay or a play button that is visisble and in the Acessiblility Tree";
+        evaluation.resultCode = 'RC3';
+      }
+
+
+      const [htmlCode, pointer] = await Promise.all([
+        DomUtils.getElementHtmlCode(element),
+        DomUtils.getElementSelector(element)
+      ]);
+      evaluation.htmlCode = htmlCode;
+      evaluation.pointer = pointer;
+
+      super.addEvaluationResult(evaluation);
+    }
+  }
+}
+
+export = QW_ACT_R29;
--- packages/act-rules/src/rules/mapping.ts
+++ packages/act-rules/src/rules/mapping.ts
@@ -27,6 +27,7 @@ export default {
       '[role]': ['QW-ACT-R20','QW-ACT-R33'],
       'img, [role="img"]':  ['QW-ACT-R17'],
       'iframe': ['QW-ACT-R19'],
+      'audio':['QW-ACT-R29'],
       '[aria-label], [aria-labelledby]': ['QW-ACT-R30'],
       'video': ['QW-ACT-R23', 'QW-ACT-R26', 'QW-ACT-R31'],
       'audio, video': ['QW-ACT-R15'],
