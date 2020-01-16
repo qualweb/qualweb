daced149811ff175a41326f447e148980ab8264a
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-01-16 17:09:49 +0000

    html integration and get element from selector

--- packages/util/src/shadowDomUtils/processShadowDom.ts
+++ packages/util/src/shadowDomUtils/processShadowDom.ts
@@ -3,65 +3,73 @@
 import { Page } from 'puppeteer';
 
 async function processShadowDom(page: Page): Promise<Page> {
-  page.evaluate((elem) => {
+  let selectors = await page.evaluate((elem) => {
 
-     function getElementSelector(elem) {   
-        function getSelfLocationInParent(element) {
-          let selector = '';
+    function getElementSelector(elem) {
+      function getSelfLocationInParent(element) {
+        let selector = '';
 
-          if (element.tagName.toLowerCase() === 'body' || element.tagName.toLowerCase() === 'head') {
-            return element.tagName.toLowerCase();
+        if (element.tagName.toLowerCase() === 'body' || element.tagName.toLowerCase() === 'head') {
+          return element.tagName.toLowerCase();
+        }
+
+        let sameEleCount = 0;
+
+        let prev = element.previousElementSibling;
+        while (prev) {
+          if (prev.tagName.toLowerCase() === element.tagName.toLowerCase()) {
+            sameEleCount++;
           }
-
-          let sameEleCount = 0;
-
-          let prev = element.previousElementSibling;
-          while (prev) {
-            if (prev.tagName.toLowerCase() === element.tagName.toLowerCase()) {
-              sameEleCount++;
-            }
-            prev = prev.previousElementSibling;
-          }
-
-          selector += `${element.tagName.toLowerCase()}:nth-of-type(${sameEleCount + 1})`;
-
-          return selector;
+          prev = prev.previousElementSibling;
         }
 
-        if (elem.tagName.toLowerCase() === 'html') {
-          return 'html';
-        } else if (elem.tagName.toLowerCase() === 'head') {
-          return 'html > head';
-        } else if (elem.tagName.toLowerCase() === 'body') {
-          return 'html > body';
-        }
-
-        let selector = 'html > ';
-        let parents = new Array<string>();
-        let parent = elem['parentElement'];
-        while (parent && parent.tagName.toLowerCase() !== 'html') {
-          parents.unshift(getSelfLocationInParent(parent));
-          parent = parent['parentElement'];
-        }
-
-        selector += parents.join(' > ');
-        selector += ' > ' + getSelfLocationInParent(elem);
+        selector += `${element.tagName.toLowerCase()}:nth-of-type(${sameEleCount + 1})`;
 
         return selector;
       }
-    
 
+      if (elem.tagName.toLowerCase() === 'html') {
+        return 'html';
+      } else if (elem.tagName.toLowerCase() === 'head') {
+        return 'html > head';
+      } else if (elem.tagName.toLowerCase() === 'body') {
+        return 'html > body';
+      }
 
+      let selector = 'html > ';
+      let parents = new Array<string>();
+      let parent = elem['parentElement'];
+      while (parent && parent.tagName.toLowerCase() !== 'html') {
+        parents.unshift(getSelfLocationInParent(parent));
+        parent = parent['parentElement'];
+      }
+
+      selector += parents.join(' > ');
+      selector += ' > ' + getSelfLocationInParent(elem);
+
+      return selector;
+    }
 
     let listElements = document.querySelectorAll("*");
-    let listOfSelectors;
+    let listOfSelectors:string [] = [];
+
 
     for (let element of listElements) {
       if (element.shadowRoot !== null) {
-        listOfSelectors.push( getElementSelector(element))
+        element.innerHTML =element.shadowRoot.innerHTML;
+        listOfSelectors.push(getElementSelector(element));
+        
       }
     }
+
+    return listOfSelectors;
   });
+  let shadowRoot;
+  for (let selector of selectors) { 
+    shadowRoot = await page.$(selector);
+  }
+
+
 
   return page;
 }
