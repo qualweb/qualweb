0e0f333482926567afa0864866f66dbc8e8af284
Andre Santos <andre13521@gmail.com>
2020-02-27 16:53:02 +0000

    fix missing cases for getDisabledWidgets

--- packages/util/src/accessibilityUtils/getDisabledWidgets.ts
+++ packages/util/src/accessibilityUtils/getDisabledWidgets.ts
@@ -3,17 +3,26 @@
 import {ElementHandle, Page} from "puppeteer";
 import isElementWidget from './isElementWidget';
 import getElementAttribute from '../domUtils/getElementAttribute';
+import getElementParent from '../domUtils/getElementParent'
+import getElementTagName from '../domUtils/getElementTagName'
 
 async function getDisabledWidgets(page:Page): Promise<ElementHandle[]> {
   let elements = await page.$$('*');
   let disabledElements:ElementHandle[] = [];
-  let isWidget;
-  let disable;
-  let ariaDisable;
+  let isWidget, disable, ariaDisable, parent, parentTag;
   for(let element of elements){
     isWidget = await isElementWidget(element,page);
     disable = (await getElementAttribute(element, 'disabled')) !== null;
     ariaDisable = (await getElementAttribute(element, 'aria-disabled')) !== null;
+    parent = await getElementParent(element);
+    if(parent && !(disable || ariaDisable)){
+      parentTag = await getElementTagName(parent);
+      if(parentTag === "label"){
+        parent = await getElementParent(parent);
+        disable = (await getElementAttribute(parent, 'disabled')) !== null;
+        ariaDisable = (await getElementAttribute(parent, 'aria-disabled')) !== null;
+      }
+    }
     if(isWidget && (disable || ariaDisable)){
       disabledElements.push(element);
     }
