9cfc1c4a855ee9b51f23eab57892543fb690264d
João Vicente <joao.vicente@campus.ul.pt>
2020-03-18 15:21:40 +0000

    updated util module interfaces

--- packages/types/util.d.ts
+++ packages/types/util.d.ts
@@ -72,7 +72,7 @@ declare module '@qualweb/util' {
     export function isElementReferencedByAriaLabel(element:ElementHandle, page:Page): Promise<boolean>;
     export function isElementWidget(element: ElementHandle): Promise<boolean>;
     export function getDisabledWidgets(page:Page): Promise<ElementHandle[]>;
-    export function getAccessibleNameRecursion(element: ElementHandle, page: Page, recursion: boolean, isWidget: boolean): Promise<string []| undefined>;
+    export function getAccessibleNameSelector(element: ElementHandle, page: Page): Promise<string[] | undefined>;
   }
 
   namespace CssUtils {
