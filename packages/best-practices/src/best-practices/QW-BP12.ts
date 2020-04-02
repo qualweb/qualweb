549ed4d6f55a2fbcadf943edede6ab788fa4c030
AntonioEstriga <37936410+AntonioEstriga@users.noreply.github.com>
2020-04-02 15:19:55 +0100

    Update QW-BP12.ts

--- packages/best-practices/src/best-practices/QW-BP12.ts
+++ packages/best-practices/src/best-practices/QW-BP12.ts
@@ -12,7 +12,7 @@ class QW_BP12 extends BestPractice {
     super({
       name: 'Using scope col and row ',
       code: 'QW-BP12',
-      description: 'Using scope col in the first row (except first) and scope row in the first element of each row (except first)',
+      description: 'Using scope col in the first row and scope row in the first element of each row',
       metadata: {
         target: {
           element: ['table', 'tr']
