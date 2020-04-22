2c34cdf473da18c61a6d6d7c7c76acacde35ec0b
andre <andre@pop-os.localdomain>
2020-04-22 18:24:32 +0100

    print state of the crawler

--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,36 +1,50 @@
 'use strict';
 
 import Crawler from 'simplecrawler';
+const logUpdate = require('log-update');
 
 class Crawl {
 
   private urls: Array<string>;
   private crawler: Crawler;
+  private crawledURLS: number;
+  private frames = ['-', '\\', '|', '/'];
+  private i = 0;
 
   constructor(domain: string) {
     this.urls = new Array<string>();
     this.crawler = new Crawler(domain);
+    this.crawledURLS = 1;
   }
 
   public async start(options?: any): Promise<void> {
     return new Promise(resolve => {
+      console.log("Starting crawler...")
       if (options) {
         this.crawler.maxConcurrency = 100;
         this.crawler.maxDepth = 0;
         this.crawler.stripQuerystring = true;
       }
 
+      setInterval(() => {
+        const frame = this.frames[this.i = ++this.i % this.frames.length];
+        logUpdate('Crawled ' + this.crawledURLS + ' pages ' + `${frame}` );
+      }, 100);
+
       this.crawler.on('fetchcomplete', (item: any) => {
         if (item && item['stateData'] && item['stateData']['contentType'] && 
             item['stateData']['contentType'].includes('text/html') && 
             !this.urls.includes(item.url)) {
           this.urls.push(item.url);
+          const frame = this.frames[this.i = ++this.i % this.frames.length];
+          logUpdate('Crawled ' + this.crawledURLS++ + ' pages ' + `${frame}`);
         }
       });
 
       this.crawler.on('complete', () => {
         this.stop();
         resolve();
+        console.log("Crawler done!");
       });
 
       this.crawler.start();
