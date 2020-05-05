b28747d4c0d8cdc87c2cfec71fb97fdaec30687f
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-05-05 17:02:06 +0100

    bug fixes

--- packages/qw-page/src/index.ts
+++ packages/qw-page/src/index.ts
@@ -7,19 +7,29 @@ class QWPage {
   }
   public getElement(selector: string): QWElement | null {
 
-    let element = document.querySelector(selector)
+    let element = this.document.querySelector(selector)
     if (element)
       return new QWElement(element);
     else
       return null;
   }
   public getElements(selector: string): Array<QWElement> {
-    let elements = document.querySelectorAll(selector)
+    let elements = this.document.querySelectorAll(selector)
     let qwList: Array<QWElement> = [];
     for (let element of elements) {
       qwList.push(new QWElement(element));
     }
     return qwList;
+
+  }
+
+  public getElementByID(id: string,elementQW: QWElement):QWElement | null {
+     let treeSelector = elementQW.getTreeSelector();
+     let element = this.document.querySelector(`#${id}`+treeSelector);
+    if (element)
+      return new QWElement(element);
+    else
+      return null;
 
   }
 }
