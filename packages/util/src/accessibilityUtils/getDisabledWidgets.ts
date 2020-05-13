e63d68057634bb52b5ffc0d891d795d76134f51c
Bruno Andrade <brunocea.1@gmail.com>
2020-05-13 17:55:07 +0100

    fixed parent bug in getDisabledWidgets

--- packages/util/src/accessibilityUtils/getDisabledWidgets.ts
+++ packages/util/src/accessibilityUtils/getDisabledWidgets.ts
@@ -5,7 +5,7 @@ import { QWElement } from '@qualweb/qw-element';
 import isElementWidget from './isElementWidget';
 
 function getDisabledWidgets(pageQW: QWPage): QWElement[] {
-  let elements = pageQW.getElements( '*');
+  let elements = pageQW.getElements('*');
   let disabledElements: QWElement[] = [];
   let isWidget, disable, ariaDisable, parent, parentTag;
   for (let element of elements) {
@@ -14,11 +14,11 @@ function getDisabledWidgets(pageQW: QWPage): QWElement[] {
     ariaDisable = (element.getElementAttribute( 'aria-disabled')) !== null;
     parent = element.getElementParent();
     if (parent && !(disable || ariaDisable)) {
-      parentTag = element.getElementTagName();
+      parentTag = parent.getElementTagName();
       if (parentTag === "label") {
-        parent = element.getElementParent();
-        disable = (element.getElementAttribute( 'disabled')) !== null;
-        ariaDisable = (element.getElementAttribute( 'aria-disabled')) !== null;
+        parent = parent.getElementParent();
+        disable = (parent.getElementAttribute( 'disabled')) !== null;
+        ariaDisable = (parent.getElementAttribute( 'aria-disabled')) !== null;
       }
     }
     if (isWidget && (disable || ariaDisable)) {
