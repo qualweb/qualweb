7909a0a4bbe0e57f4e3596334cbcabc7f0a0d235
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-05-15 16:41:56 +0100

    fixed role none conflict

--- packages/util/src/accessibilityUtils/getElementRoleAName.ts
+++ packages/util/src/accessibilityUtils/getElementRoleAName.ts
@@ -2,13 +2,14 @@
 import { QWPage } from '@qualweb/qw-page';
 import { QWElement } from '@qualweb/qw-element';
 import getImplicitRole from './getImplicitRole';
+import isElementFocusable from '../domUtils/isElementFocusable';
 
-function getElementRoleAName(elementQW: QWElement, pageQW: QWPage, aName: string|undefined): string | null {
-  let explicitRole = elementQW.getElementAttribute( "role");
+function getElementRoleAName(elementQW: QWElement, pageQW: QWPage, aName: string | undefined): string | null {
+  let explicitRole = elementQW.getElementAttribute("role");
   let role = explicitRole;
-  if (explicitRole === null) {
-    role = getImplicitRole(elementQW,pageQW,aName);
-    }
+  if (explicitRole === null || ((explicitRole === "none" || explicitRole === "presentation") && isElementFocusable(elementQW))) {
+    role = getImplicitRole(elementQW, pageQW, aName);
+  }
   return role;
 }
 
