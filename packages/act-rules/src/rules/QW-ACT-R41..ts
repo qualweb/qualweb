030e670ac3ca5ecee496f1d07216555bf7f3df1a
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-05-28 15:39:00 +0100

    code done

---
+++ packages/act-rules/src/rules/QW-ACT-R41..ts
@@ -0,0 +1,32 @@
+'use strict';
+
+import { ACTRuleResult } from '@qualweb/act-rules';
+import Rule from '../lib/Rule.object';
+import { ACTRule, ElementExists, ElementHasOneOfTheFollowingRoles } from '../lib/decorator';
+import { QWElement } from "@qualweb/qw-element";
+
+@ACTRule
+class QW_ACT_R41 extends Rule {
+
+  constructor(rule?: any) {
+    super(rule);
+  }
+
+  @ElementExists
+  @ElementHasOneOfTheFollowingRoles(["checkbox", "combobox", "listbox", "menuitemcheckbox", "menuitemradio", "radio", "searchbox", "slider", "spinbutton", "switch", "textbox"])
+  execute(element: QWElement): void {
+    const evaluation: ACTRuleResult = {
+      verdict: '',
+      description: '',
+      resultCode: ''
+    };
+
+    evaluation.verdict = 'warning';
+    evaluation.description = ' Check that text error messages provided, identify the cause of the error or how to fix the error.';
+    evaluation.resultCode = 'RC1';
+
+    super.addEvaluationResult(evaluation, element);
+  }
+}
+
+export = QW_ACT_R41;
