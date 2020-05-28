b71722a630391e5072f49de8d54a59e7eb2bacb1
João Vicente <joao.vicente@campus.ul.pt>
2020-05-28 16:56:36 +0100

    updated dependencies

--- packages/qw-page/src/index.ts
+++ packages/qw-page/src/index.ts
@@ -1,71 +1,72 @@
 import { QWElement } from '@qualweb/qw-element';
 
 class QWPage {
+
   private document: Document;
-  constructor(document: Document) {
+  private window: Window;
+
+  private defaultWidth: number;
+  private defaultHeight: number;
+
+  constructor(document: Document, window: Window) {
     this.document = document;
+    this.window = window;
+
+    this.defaultWidth = this.window.innerWidth;
+    this.defaultHeight = this.window.innerHeight;
   }
 
   public getURL(): string {
     return this.document.URL;
   }
-  public getElement(selector: string): QWElement | null {
 
-    let element = this.document.querySelector(selector)
-    if (element)
-      return new QWElement(element);
-    else
-      return null;
+  public getElement(selector: string): QWElement | null {
+    const element = this.document.querySelector(selector);
+    return element ? new QWElement(element) : null;
   }
+
   public getElements(selector: string): Array<QWElement> {
-    let elements = this.document.querySelectorAll(selector)
-    let qwList: Array<QWElement> = [];
-    for (let element of elements) {
+    const elements = this.document.querySelectorAll(selector);
+    const qwList: Array<QWElement> = [];
+
+    for (const element of elements || []) {
       qwList.push(new QWElement(element));
     }
-    return qwList;
 
+    return qwList;
   }
 
   public getElementByID(id: string, elementQW: QWElement): QWElement | null {
-    let treeSelector = elementQW.getTreeSelector();
-    let element = this.document.querySelector(`#${id}` + treeSelector);
-    if (element)
-      return new QWElement(element);
-    else
-      return null;
-
+    const treeSelector = elementQW.getTreeSelector();
+    const element = this.document.querySelector(`#${id}` + treeSelector);
+    return element ? new QWElement(element) : null;
   }
+
   public getElementByAttributeName(name: string): QWElement | null {
-    let element = this.document.querySelector(`[name="${name}"]`);
-    if (element)
-      return new QWElement(element);
-    else
-      return null;
-
+    const element = this.document.querySelector(`[name="${name}"]`);
+    return element ? new QWElement(element) : null;
   }
+
   public processShadowDom(): void {
-
-
-    let listElements = this.document.querySelectorAll("*") || new Array();
-    let elementsFromShadowDom;
+    const listElements = this.document.querySelectorAll('*');
     let shadowCounter = 0;
 
-    listElements.forEach(element => {
+    for (const element of listElements || []) {
       if (element.shadowRoot !== null) {
         element.innerHTML = element.shadowRoot.innerHTML;
-        elementsFromShadowDom = element.querySelectorAll("*");
+        const elementsFromShadowDom = element.querySelectorAll('*');
         this.setShadowAttribute(elementsFromShadowDom, shadowCounter);
         shadowCounter++;
       }
-    });
+    }
   }
 
   private setShadowAttribute(elements: NodeListOf<Element>, counter: number): void {
     for (const element of elements || []) {
-      element.setAttribute("shadowTree", counter + "")
+      element.setAttribute('shadowTree', counter + '');
     }
   }
+  
   public getPageRootElement(): QWElement | null {
     const documentElement = this.document.documentElement;
     return documentElement ? new QWElement(documentElement) : null;
@@ -76,13 +77,16 @@ class QWPage {
   }
 
   public getFocusedElement(): QWElement | null {
-    let activeElement = this.document.activeElement;
-    let result;
-    if (activeElement)
-      result = new QWElement(activeElement);
-    return result;
-
+    const activeElement = this.document.activeElement;
+    return activeElement ? new QWElement(activeElement) : null
   }
 
+  public changeToDefaultViewport(): void {
+    this.window.resizeTo(this.defaultWidth, this.defaultHeight);
+  }
+
+  public changeViewport(width: number, height: number): void {
+    this.window.resizeTo(width, height);
+  }
 }
 export { QWPage };
