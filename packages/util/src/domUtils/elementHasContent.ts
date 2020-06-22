56b9cc26d3d3e763bea1149cf38af71d73d07254
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-06-22 18:18:03 +0100

    new version

--- packages/util/src/domUtils/elementHasContent.ts
+++ packages/util/src/domUtils/elementHasContent.ts
@@ -1,18 +1,17 @@
 'use strict';
 import { QWElement } from "@qualweb/qw-element";
-import { sectionAndGrouping } from "../accessibilityUtils/constants";
 
 function elementHasContent(elementQW: QWElement): boolean {
   if (!elementQW) {
     throw Error('Element is not defined');
   }
   let text = elementQW.getElementText();
-  const sectionOrGrouping =isElementGroupingOrSection(elementQW);
+ // const sectionOrGrouping =isElementGroupingOrSection(elementQW);
   if(text)
     text = text.trim();
-return !sectionOrGrouping || !!text;
+return /*!sectionOrGrouping ||*/ !!text;
 }
-function isElementGroupingOrSection(elementQW:QWElement):boolean{
+/*function isElementGroupingOrSection(elementQW:QWElement):boolean{
   let tagName =  elementQW.getElementTagName();
   let isGroupingOrSection = sectionAndGrouping.includes(tagName);
   let children= elementQW.getElementChildren();
@@ -22,6 +21,6 @@ function isElementGroupingOrSection(elementQW:QWElement):boolean{
     counter++;
   }
   return isGroupingOrSection;
-}
+}*/
 
 export default elementHasContent;
