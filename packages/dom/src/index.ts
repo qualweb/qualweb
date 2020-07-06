d4d17f6ff187b2e73cb89ab1348ca89909c668b1
João Vicente <joao.vicente@campus.ul.pt>
2020-07-06 16:18:44 +0100

    Updated dependencies. Code optimization

--- packages/dom/src/index.ts
+++ packages/dom/src/index.ts
@@ -1,4 +1,3 @@
-import CSSselect from 'css-select';
 import {
   Browser,
   Page,
@@ -8,18 +7,15 @@ import {
 import {
   QualwebOptions,
   SourceHtml,
-  PageOptions,
-  CSSStylesheet
+  PageOptions
 } from '@qualweb/core';
 import {
   PageData
 } from '@qualweb/dom';
 import fetch from 'node-fetch';
-import clone from 'lodash.clone';
 import DomHandler, {
   Node
 } from 'domhandler';
-import css from 'css';
 import {
   Parser
 } from 'htmlparser2';
@@ -43,15 +39,6 @@ class Dom {
       await this.page.setBypassCSP(true);
       await this.setPageViewport(options.viewport);
 
-      const plainStylesheets: any = {};
-      this.page.on('response', async response => {
-        if (response.request().resourceType() === 'stylesheet') {
-          const responseUrl = response.url();
-          const content = await response.text();
-          plainStylesheets[responseUrl] = content;
-        }
-      });
-
       let _sourceHtml = '';
 
       if (url) {
@@ -62,12 +49,8 @@ class Dom {
           waitUntil: ['networkidle2', 'domcontentloaded']
         });
         
-        let sourceHTMLPuppeteer = '';
+        const sourceHTMLPuppeteer = await response?.text();
         
-        if (response) {
-          sourceHTMLPuppeteer = await response.text();
-        }
-
         if (this.isSVGorMath(sourceHTMLPuppeteer)) {
           this.page.close()
           this.page = await browser.newPage();
@@ -82,50 +65,22 @@ class Dom {
           waitUntil: ['networkidle2', 'domcontentloaded']
         });
         _sourceHtml = await this.page.content();
+
+        if (this.isSVGorMath(_sourceHtml)) {
+          this.page.close()
+          this.page = await browser.newPage();
+          await this.page.setContent('<!DOCTYPE html><html nonHTMLPage=true><body></body></html>', {
+            timeout: 0,
+            waitUntil: ['networkidle2', 'domcontentloaded']
+          });
+        }
       }
 
       const sourceHtml = await this.parseSourceHTML(_sourceHtml);
-      const styles = CSSselect('style', sourceHtml.html.parsed);
-      const allElements = CSSselect('[style]', sourceHtml.html.parsed);
-      let k = 0;
-
-      for (const style of styles || []) {
-        if (style['children'] && style['children'][0]) {
-          plainStylesheets['html' + k] = style['children'][0]['data'];
-        }
-        k++;
-      }
-
-      for (const elem of allElements || []) {
-        if (elem['name'] && elem['attribs'] && elem['attribs']['style']) {
-          plainStylesheets['html' + k] = elem['name'] + '{' + elem['attribs']['style'] + '}';
-        }
-        k++;
-      }
-
-      const stylesheets = await this.parseStylesheets(plainStylesheets);
-      const cookedStew = CSSselect('*', sourceHtml.html.parsed);
-
-      const mappedDOM = {};
-
-      if (cookedStew.length > 0) {
-        for (const item of cookedStew || []) {
-          if (item['startIndex']) {
-            mappedDOM[item['startIndex']] = item;
-          }
-        }
-      }
-
-      await this.mapCSSElements(sourceHtml.html.parsed, stylesheets, mappedDOM);
 
       await this.processShadowDom();
 
-      return {
-        sourceHtml,
-        page: this.page,
-        stylesheets,
-        mappedDOM
-      };
+      return { sourceHtml, page: this.page };
     } catch (err) {
       throw err;
     }
@@ -149,14 +104,14 @@ class Dom {
         width: options.mobile ? DEFAULT_MOBILE_PAGE_VIEWPORT_WIDTH : DEFAULT_DESKTOP_PAGE_VIEWPORT_WIDTH,
         height: options.mobile ? DEFAULT_MOBILE_PAGE_VIEWPORT_HEIGHT : DEFAULT_DESKTOP_PAGE_VIEWPORT_HEIGHT
       };
-      if (options.resolution) {
-        if (options.resolution.width) {
-          viewPort.width = options.resolution.width;
-        }
-        if (options.resolution.height) {
-          viewPort.height = options.resolution.height;
-        }
+      
+      if (options.resolution?.width) {
+        viewPort.width = options.resolution.width;
       }
+      if (options.resolution?.height) {
+        viewPort.height = options.resolution.height;
+      }
+
       viewPort.isMobile = !!options.mobile;
       viewPort.isLandscape = options.landscape !== undefined ? options.landscape : viewPort.width > viewPort.height;
       viewPort.hasTouch = !!options.mobile;
@@ -180,36 +135,14 @@ class Dom {
       }
     };
     const response = await fetch(url, fetchOptions);
-    const sourceHTML = (await response.text()).trim();
-
-    return sourceHTML;
-  }
-
-  private async parseStylesheets(plainStylesheets: any): Promise<CSSStylesheet[]> {
-    const stylesheets = new Array<CSSStylesheet>();
-
-    for (const file in plainStylesheets || {}) {
-      const stylesheet: CSSStylesheet = {
-        file,
-        content: {}
-      };
-      if (stylesheet.content) {
-        stylesheet.content.plain = plainStylesheets[file];
-        stylesheet.content.parsed = css.parse(plainStylesheets[file], {
-          silent: true
-        }); //doesn't throw errors
-        stylesheets.push(clone(stylesheet));
-      }
-    }
-
-    return stylesheets;
+    return (await response.text()).trim();
   }
 
   private async parseSourceHTML(html: string): Promise<SourceHtml> {
 
-    const sourceHTML: string = html.trim();
+    const sourceHTML = html.trim();
     const parsedHTML = this.parseHTML(sourceHTML);
-    const source: SourceHtml = {
+    const source = {
       html: {
         plain: sourceHTML,
         parsed: parsedHTML
@@ -231,83 +164,11 @@ class Dom {
     return handler.dom;
   }
 
-  private async mapCSSElements(dom: Node[], styleSheets: any, mappedDOM: any): Promise < void > {
-    for (const styleSheet of styleSheets || []) {
-      if (styleSheet.content && styleSheet.content.plain) {
-        this.analyseAST(dom, styleSheet.content.parsed, undefined, mappedDOM);
-      }
-    }
-  }
-
-  private analyseAST(dom: Node[], cssObject: any, parentType: string | undefined, mappedDOM: any): void {
-    if (cssObject === undefined ||
-      cssObject['type'] === 'comment' ||
-      cssObject['type'] === 'keyframes' ||
-      cssObject['type'] === 'import') {
-      return;
-    }
-    if (cssObject['type'] === 'rule' || cssObject['type'] === 'font-face' || cssObject['type'] === 'this.page') {
-      this.loopDeclarations(dom, cssObject, parentType, mappedDOM);
-    } else {
-      if (cssObject['type'] === 'stylesheet') {
-        for (const key of cssObject['stylesheet']['rules'] || []) {
-          this.analyseAST(dom, key, undefined, mappedDOM);
-        }
-      } else {
-        for (const key of cssObject['rules'] || []) {
-          if (cssObject['type'] && cssObject['type'] === 'media') {
-            this.analyseAST(dom, key, cssObject[cssObject['type']], mappedDOM);
-          } else {
-            this.analyseAST(dom, key, undefined, mappedDOM);
-          }
-        }
-      }
-    }
-  }
-
-  private loopDeclarations(dom: Node[], cssObject: any, parentType: string | undefined, mappedDOM: any): void {
-    const declarations = cssObject['declarations'];
-    if (declarations && cssObject['selectors'] && !cssObject['selectors'].toString().includes('@-ms-viewport') && !(cssObject['selectors'].toString() === ':focus')) {
-      try {
-        let stewResult = CSSselect(cssObject['selectors'].toString(), dom);
-        if (stewResult.length > 0) {
-          for (const item of stewResult || []) {
-            if (item['startIndex']) {
-              for (const declaration of declarations || []) {
-                if (declaration['property'] && declaration['value']) {
-                  if (!item['attribs']) {
-                    item['attribs'] = {};
-                  }
-                  if (!item['attribs']['css']) {
-                    item['attribs']['css'] = {};
-                  }
-                  if (item['attribs']['css'][declaration['property']] && item['attribs']['css'][declaration['property']]['value'] &&
-                    item['attribs']['css'][declaration['property']]['value'].includes('!important')) {
-                    continue;
-                  } else {
-                    item['attribs']['css'][declaration['property']] = {};
-                    if (parentType) {
-                      item['attribs']['css'][declaration['property']]['media'] = parentType;
-                    }
-                    item['attribs']['css'][declaration['property']]['value'] = declaration['value'];
-                  }
-                  mappedDOM[item['startIndex']] = item;
-                }
-              }
-            }
-          }
-        }
-      } catch (err) {
-
-      }
-    }
-  }
-
   private async processShadowDom(): Promise<void> {
-    let selectors = await this.page.evaluate((elem) => {
+    const selectors = await this.page.evaluate((elem: Element) => {
 
-      function getElementSelector(elem) {
-        function getSelfLocationInParent(element) {
+      function getElementSelector(elem: Element): string {
+        function getSelfLocationInParent(element: Element): string {
           let selector = '';
 
           if (element.tagName.toLowerCase() === 'body' || element.tagName.toLowerCase() === 'head') {
@@ -338,7 +199,7 @@ class Dom {
         }
 
         let selector = 'html > ';
-        let parents = new Array < string > ();
+        const parents = new Array<string>();
         let parent = elem['parentElement'];
         while (parent && parent.tagName.toLowerCase() !== 'html') {
           parents.unshift(getSelfLocationInParent(parent));
@@ -351,7 +212,7 @@ class Dom {
         return selector;
       }
 
-      const listElements = document.querySelectorAll('*') || new Array();
+      const listElements = document.querySelectorAll('*') || new Array<Element>();
       const listOfSelectors = new Array<string>();
 
       listElements.forEach(element => {
@@ -383,8 +244,8 @@ class Dom {
     }
   }
 
-  private isSVGorMath(content: string): boolean {
-    return content.trim().startsWith('<math') || content.trim().startsWith('<svg');
+  private isSVGorMath(content?: string): boolean {
+    return !!(content?.trim().startsWith('<math') || content?.trim().startsWith('<svg'));
   }
 }
 
