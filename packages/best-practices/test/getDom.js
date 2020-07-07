802486d2ae879e316c10c14e4484e070f1b00642
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-07-07 15:10:52 +0100

    playwright test

--- packages/best-practices/test/getDom.js
+++ packages/best-practices/test/getDom.js
@@ -2,11 +2,18 @@ const constants = require('./constants')
 const clone = require("lodash/clone");
 const css = require("css");
 const htmlparser2 = require("htmlparser2");
-const request =require("request");
-const stew = new (require('stew-select')).Stew();
+const fetch = require("node-fetch");
+const CSSselect = require('css-select');
 
-async function getDom(browser,url) {
+async function getDom(browser,url, viewport) {
     const page = await browser.newPage();
+
+    //await page.setBypassCSP(true);
+
+    if (viewport) {
+        await page.setViewport(viewport);
+    }
+
     const plainStylesheets = {};
     page.on('response', async response => {
         if (response.request().resourceType() === 'stylesheet') {
@@ -17,12 +24,14 @@ async function getDom(browser,url) {
     });
 
     await page.goto(url, {
-        waitUntil: ['networkidle2', 'domcontentloaded']
+        waitUntil: 'networkidle'
     });
 
+    
     const sourceHtml = await getSourceHTML(url);
+    //let stylesheets = [];
 
-    let styles = stew.select(sourceHtml.html.parsed, 'style');
+    let styles = CSSselect('style', sourceHtml.html.parsed);
     for (let i = 0; i < styles.length; i++) {
         if (styles[i]['children'][0]) {
             plainStylesheets['html' + i] = styles[i]['children'][0]['data'];
@@ -32,16 +41,18 @@ async function getDom(browser,url) {
     const stylesheets = await parseStylesheets(plainStylesheets);
 
     const mappedDOM = {};
-    const cookedStew = await stew.select(sourceHtml.html.parsed, '*');
+    const cookedStew = CSSselect('*', sourceHtml.html.parsed);
     if (cookedStew.length > 0)
         for (const item of cookedStew || [])
-            mappedDOM[item['_stew_node_id']] = item;
+            mappedDOM[item['_node_id']] = item;
 
     await mapCSSElements(sourceHtml.html.parsed, stylesheets, mappedDOM);
 
-    return { sourceHtml, page, stylesheets, mappedDOM };
+    return { sourceHtml, page, stylesheets };
 }
+
 async function parseStylesheets(plainStylesheets) {
+
     const stylesheets = new Array();
     for (const file in plainStylesheets || {}) {
         const stylesheet = { file, content: {} };
@@ -53,34 +64,26 @@ async function parseStylesheets(plainStylesheets) {
     }
     return stylesheets;
 }
-async function getRequestData(headers) {
-    return new Promise((resolve, reject) => {
-        request(headers, (error, response, body) => {
-            if (error) {
-                reject(error);
-            }
-            else if (!response || response.statusCode !== 200) {
-                reject(response.statusCode);
-            }
-            else {
-                resolve({ response, body });
-            }
-        });
-    });
+
+
+async function getTestCases() {
+  const response = await fetch('https://act-rules.github.io/testcases.json');
+  return await response.json();
 }
+
 async function getSourceHTML(url, options) {
-    const headers = {
-        'url': url,
+    const fetchOptions = {
         'headers': {
             'User-Agent': options ? options.userAgent ? options.userAgent : options.mobile ? constants.DEFAULT_MOBILE_USER_AGENT : constants.DEFAULT_DESKTOP_USER_AGENT : constants.DEFAULT_DESKTOP_USER_AGENT
         }
     };
-    const data = await getRequestData(headers);
-    const sourceHTML = data.body.toString().trim();
+    const response = await fetch(url, fetchOptions);
+    //const data = await response.text();
+    const sourceHTML = (await response.text()).trim();
     const parsedHTML = parseHTML(sourceHTML);
-    const elements = stew.select(parsedHTML, '*');
+    const elements = CSSselect('*', parsedHTML);
     let title = '';
-    const titleElement = stew.select(parsedHTML, 'title');
+    const titleElement = CSSselect('title', parsedHTML);
     if (titleElement.length > 0) {
         title = htmlparser2.DomUtils.getText(titleElement[0]);
     }
@@ -112,70 +115,72 @@ function parseHTML(html) {
     }
     return parsed;
 }
+
 async function mapCSSElements(dom, styleSheets, mappedDOM) {
-    for (const styleSheet of styleSheets || [])
-        if (styleSheet.content && styleSheet.content.plain)
-            analyseAST(dom, styleSheet.content.parsed, undefined, mappedDOM);
+  for (const styleSheet of styleSheets || [])
+      if (styleSheet.content && styleSheet.content.plain)
+          analyseAST(dom, styleSheet.content.parsed, undefined, mappedDOM);
 }
 function analyseAST(dom, cssObject, parentType, mappedDOM) {
-    if (cssObject === undefined ||
-        cssObject['type'] === 'comment' ||
-        cssObject['type'] === 'keyframes' ||
-        cssObject['type'] === 'import') {
-        return;
-    }
-    if (cssObject['type'] === 'rule' || cssObject['type'] === 'font-face' || cssObject['type'] === 'page') {
-        loopDeclarations(dom, cssObject, parentType, mappedDOM);
-    }
-    else {
-        if (cssObject['type'] === 'stylesheet') {
-            for (const key of cssObject['stylesheet']['rules'] || []) {
-                analyseAST(dom, key, undefined, mappedDOM);
-            }
-        }
-        else {
-            for (const key of cssObject['rules'] || []) {
-                if (cssObject['type'] && cssObject['type'] === 'media')
-                    analyseAST(dom, key, cssObject[cssObject['type']], mappedDOM);
-                else
-                    analyseAST(dom, key, undefined, mappedDOM);
-            }
-        }
-    }
+  if (cssObject === undefined ||
+      cssObject['type'] === 'comment' ||
+      cssObject['type'] === 'keyframes' ||
+      cssObject['type'] === 'import') {
+      return;
+  }
+  if (cssObject['type'] === 'rule' || cssObject['type'] === 'font-face' || cssObject['type'] === 'page') {
+      loopDeclarations(dom, cssObject, parentType, mappedDOM);
+  }
+  else {
+      if (cssObject['type'] === 'stylesheet') {
+          for (const key of cssObject['stylesheet']['rules'] || []) {
+              analyseAST(dom, key, undefined, mappedDOM);
+          }
+      }
+      else {
+          for (const key of cssObject['rules'] || []) {
+              if (cssObject['type'] && cssObject['type'] === 'media')
+                  analyseAST(dom, key, cssObject[cssObject['type']], mappedDOM);
+              else
+                  analyseAST(dom, key, undefined, mappedDOM);
+          }
+      }
+  }
 }
 function loopDeclarations(dom, cssObject, parentType, mappedDOM) {
-    let declarations = cssObject['declarations'];
-    if (declarations && cssObject['selectors'] && !cssObject['selectors'].toString().includes('@-ms-viewport') && !(cssObject['selectors'].toString() === ":focus")) {
-        try {
-            let stewResult = stew.select(dom, cssObject['selectors'].toString());
-            if (stewResult.length > 0) {
-                for (const item of stewResult || []) {
-                    for (const declaration of declarations || []) {
-                        if (declaration['property'] && declaration['value']) {
-                            if (!item['attribs'])
-                                item['attribs']={}
-                            if (!item['attribs']['css'])
-                                item['attribs']['css'] = {};
-                            if (item['attribs']['css'][declaration['property']] && item['attribs']['css'][declaration['property']]['value'] &&
-                                item['attribs']['css'][declaration['property']]['value'].includes("!important")) {
-                                continue;
-                            }
-                            else {
-                                item['attribs']['css'][declaration['property']] = {};
-                                if (parentType) {
-                                    item['attribs']['css'][declaration['property']]['media'] = parentType;
-                                }
-                                item['attribs']['css'][declaration['property']]['value'] = declaration['value'];
-                            }
-                            mappedDOM[item['_stew_node_id']] = item;
-                        }
-                    }
-                }
-            }
-        }
-        catch (err) {
-        }
-    }
+  let declarations = cssObject['declarations'];
+  if (declarations && cssObject['selectors'] && !cssObject['selectors'].toString().includes('@-ms-viewport') && !(cssObject['selectors'].toString() === ":focus")) {
+      try {
+          let stewResult = CSSselect(cssObject['selectors'].toString(), dom);
+          if (stewResult.length > 0) {
+              for (const item of stewResult || []) {
+                  for (const declaration of declarations || []) {
+                      if (declaration['property'] && declaration['value']) {
+                          if (!item['attribs'])
+                              item['attribs']={}
+                          if (!item['attribs']['css'])
+                              item['attribs']['css'] = {};
+                          if (item['attribs']['css'][declaration['property']] && item['attribs']['css'][declaration['property']]['value'] &&
+                              item['attribs']['css'][declaration['property']]['value'].includes("!important")) {
+                              continue;
+                          }
+                          else {
+                              item['attribs']['css'][declaration['property']] = {};
+                              if (parentType) {
+                                  item['attribs']['css'][declaration['property']]['media'] = parentType;
+                              }
+                              item['attribs']['css'][declaration['property']]['value'] = declaration['value'];
+                          }
+                          mappedDOM[item['_node_id']] = item;
+                      }
+                  }
+              }
+          }
+      }
+      catch (err) {
+      }
+  }
 }
 
 module.exports.getDom = getDom;
+module.exports.getTestCases = getTestCases;
