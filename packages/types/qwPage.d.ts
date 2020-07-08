d593b32281780bee0c624ba89d41df24ab2c8b0e
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-07-08 19:47:48 +0100

    changes to qwPage

--- packages/types/qwPage.d.ts
+++ packages/types/qwPage.d.ts
@@ -4,6 +4,9 @@ declare module '@qualweb/qw-page' {
 
   class QWPage {
     constructor(document: Document, window: Window);
+    public cacheValue(selector: string, method: string, value: string|undefined);
+    public getCachedValue(selector: string, method: string): string|undefined;
+    public isValueCached(selector: string, method: string): boolean;
     public getElement(selector: string): QWElement | null;
     public getElements(selector: string): Array<QWElement>;
     public getElementByID(id: string, elementQW: QWElement): QWElement | null;
