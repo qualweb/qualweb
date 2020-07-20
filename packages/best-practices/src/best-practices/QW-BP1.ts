0ff1621243e612d3216d377b7f6f0f126b479d43
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-07-20 17:22:28 +0100

    bp1 fix

--- packages/best-practices/src/best-practices/QW-BP1.ts
+++ packages/best-practices/src/best-practices/QW-BP1.ts
@@ -2,8 +2,10 @@
 
 import { BestPracticeResult } from '@qualweb/best-practices';
 import BestPracticeObject from '../lib/BestPractice.object';
-import { BestPractice } from '../lib/decorator';
+import { BestPractice, ElementExists } from '../lib/decorator';
 import { QWElement } from '@qualweb/qw-element';
+import { AccessibilityUtils, DomUtils } from '@qualweb/util';
+import { QWPage } from '@qualweb/qw-page';
 
 @BestPractice
 class QW_BP1 extends BestPracticeObject {
@@ -11,28 +13,44 @@ class QW_BP1 extends BestPracticeObject {
   constructor(bestPractice?: any) {
     super(bestPractice);
   }
+  @ElementExists
+  execute(element: QWElement | undefined, page: QWPage): void {
+    if (!element) { return }
 
-  execute(element: QWElement | undefined): void {
 
-    const evaluation: BestPracticeResult = {
-      verdict: '',
-      description: '',
-      resultCode: ''
-    };
+    let headings = element.getElements('h1, h2, h3, h4, h5, h6,[role="heading"]');
+    let hasWarning = false;
 
-    if (!element) {
+    for (let heading of headings) {
+      const evaluation: BestPracticeResult = {
+        verdict: '',
+        description: '',
+        resultCode: ''
+      };
+
+      if (!AccessibilityUtils.isElementInAT(heading, page) || DomUtils.isElementVisible(heading, page)) {
+        evaluation.verdict = 'warning';
+        evaluation.description = 'Check that heading markup is used when content is a heading';
+        evaluation.resultCode = 'RC1';
+        evaluation.htmlCode = element.getElementHtmlCode(true, true);
+        evaluation.pointer = element.getElementSelector();
+        hasWarning = true;
+      }
+      super.addEvaluationResult(evaluation);
+    }
+    if (!hasWarning) {
+      const evaluation: BestPracticeResult = {
+        verdict: '',
+        description: '',
+        resultCode: ''
+      };
       evaluation.verdict = 'failed';
       evaluation.description = `This page doesn't use headings`;
-      evaluation.resultCode = 'RC1';
-    } else {
-      evaluation.verdict = 'warning';
-      evaluation.description = 'Check that heading markup is used when content is a heading';
       evaluation.resultCode = 'RC2';
-      evaluation.htmlCode = element.getElementHtmlCode( true, true);
-      evaluation.pointer = element.getElementSelector();
+      super.addEvaluationResult(evaluation);
     }
-    
-    super.addEvaluationResult(evaluation);
+
+
   }
 }
 
--- packages/best-practices/src/lib/mapping.ts
+++ packages/best-practices/src/lib/mapping.ts
@@ -4,10 +4,10 @@ export default {
   'table': ['QW-BP9', 'QW-BP12'],
   'table table': ['QW-BP5'],
   ':not(svg) > title': ['QW-BP6', 'QW-BP7'],
-  'h1, h2, h3, h4, h5, h6,[role="heading"]': ['QW-BP1', 'QW-BP8'],
+  'h1, h2, h3, h4, h5, h6,[role="heading"]': ['QW-BP8'],
   'basefont, big, blink, center, font, marquee, multicol, nobr, spacer, tt': ['QW-BP10'],
   'body, body *': ['QW-BP11'],
-  'body': ['QW-BP16'],
+  'body': ['QW-BP16','QW-BP1'],
   'a > img': ['QW-BP13'],
   'a:first-child': ['QW-BP4'],
   '': ['QW-BP14', 'QW-BP15']
