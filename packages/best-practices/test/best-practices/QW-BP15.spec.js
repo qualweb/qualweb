7020571c54662c285f85f486b4c50de6069753a6
arbsantos <andre13521@gmail.com>
2020-07-21 14:32:58 +0100

    changed applicability to exclude images

--- packages/best-practices/test/best-practices/QW-BP15.spec.js
+++ packages/best-practices/test/best-practices/QW-BP15.spec.js
@@ -1,21 +1,27 @@
 const { BestPractices } = require('../../dist/index');
 const { expect } = require('chai');
-const puppeteer = require('puppeteer');
+const playwright = require('playwright');
 const { getDom } = require('../getDom');
 describe('Technique QW-BP15', function () {
   const tests = [
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/BP15/test1.html',
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~asantos/BP15/test1.html',
       outcome: 'failed'
     },
     {
-      url: 'https://accessible-serv.lasige.di.fc.ul.pt/~asantos/BP15/test2.html',
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~asantos/BP15/test2.html',
       outcome: 'passed'
+    },
+    {
+      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~asantos/BP15/test3.html',
+      outcome: 'inapplicable'
     }
   ];
-  let browser;
+  let browser, context;
   it("pup open", async function () {
-    browser = await puppeteer.launch();
+    browser = await playwright['chromium'].launch({headless:false});
+    context = await browser.newContext({bypassCSP:true});
+
   });
   let i = 0;
   let lastOutcome = 'warning';
@@ -28,26 +34,34 @@ describe('Technique QW-BP15', function () {
     describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
       it(`should have outcome="${test.outcome}"`, async function () {
         this.timeout(10 * 1000);
-        const { sourceHtml, page, stylesheets } = await getDom(browser, test.url);
-        await page.addScriptTag({
-          path: require.resolve('@qualweb/qw-page').replace('index.js', 'qwPage.js')
-        })
+        const { sourceHtml, page, stylesheets } = await getDom(context, test.url);
         await page.addScriptTag({
           path: require.resolve('../../dist/bp.js')
         })
-        const report = await page.evaluate(( rules,stylesheets) => {
+        await page.addScriptTag({
+          path: require.resolve('@qualweb/qw-page').replace('index.js', 'qwPage.js')
+        })
+  
+        // sourceHtml.html.parsed = {};
+        console.log("Evaluating")
+        const report = await page.evaluate((rules, stylesheets) => {
           const bp = new BestPractices.BestPractices(rules);
-          let report= bp.execute(new QWPage.QWPage(document),stylesheets);
+          const report = bp.execute(new QWPage.QWPage(document, window, true),stylesheets);
           return report;
-        }, {bestPractices: ['QW-BP15']},stylesheets);
-
+        }, ['QW-BP15']);
+        // const report = await page.evaluate(( rules,stylesheets) => {
+        //   const bp = new BestPractices.BestPractices(rules);
+        //   let report= bp.execute(new QWPage.QWPage(document),stylesheets);
+        //   return report;
+        // }, {bestPractices: ['QW-BP15']},stylesheets);
+        console.log(JSON.stringify(report['assertions']['QW-BP15'], null, 2))
         expect(report['assertions']['QW-BP15'].metadata.outcome).to.be.equal(test.outcome);
       });
     });
   }
   describe(``,  function () {
     it(`pup shutdown`, async function () {
-      await browser.close();
+      // await browser.close();
     });
   });
 });
