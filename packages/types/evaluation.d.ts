485f183d90d4c3b07606ff5f0066b1b303845073
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-09-03 17:31:40 +0100

    new version

--- packages/types/evaluation.d.ts
+++ packages/types/evaluation.d.ts
@@ -7,16 +7,16 @@ declare module '@qualweb/evaluation' {
   import { Page } from 'puppeteer';
 
   class Evaluation {
-    public evaluatePage(sourceHtml: SourceHtml, page: Page, execute: any, options: QualwebOptions, url: string): Promise<EvaluationRecord>;
+    public evaluatePage(sourceHtml: SourceHtml, page: Page, execute: any, options: QualwebOptions, url: string, validation: any): Promise<EvaluationRecord>;
     public addQWPage(page: Page): Promise<void>;
     public executeBP(page: Page, options: QualwebOptions): Promise<BestPracticesReport>;
     public executeCSS(page: Page, options: QualwebOptions): Promise<CSSTechniquesReport>;
-    public executeHTML(page: Page, options: QualwebOptions): Promise<HTMLTechniquesReport>;
+    public executeHTML(page: Page, options: QualwebOptions, validation: any): Promise<HTMLTechniquesReport>;
     public executeACT(page: Page, sourceHtml: SourceHtml, options: QualwebOptions): Promise<ACTRulesReport>;
     public getEvaluator(page: Page, sourceHtml: SourceHtml, url: string): Promise<Evaluator>;
   }
   class EvaluationRecord {
-  
+
     constructor(evaluator: Evaluator);
     public addModuleEvaluation(module: any, evaluation: any): void;
     public getFinalReport(): EvaluationReport;
