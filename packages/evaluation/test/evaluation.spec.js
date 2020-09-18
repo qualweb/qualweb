d14bfafe64b4a97566d0f606de3d009fe5890d87
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-09-18 16:58:33 +0100

    new version

--- packages/evaluation/test/evaluation.spec.js
+++ packages/evaluation/test/evaluation.spec.js
@@ -6,11 +6,11 @@ describe('QualWeb page', function() {
   it('Testing qualweb page evaluation', async function() {
     this.timeout(1000 * 1000);
 
-    const url = 'https://www.dagbladet.no/pluss';
+    const url = 'https://www.pcdiga.com/';
 
     const browser = await puppeteer.launch();
     const dom = new Dom();
-    const { sourceHtml, page ,validation} = await dom.getDOM(browser, { viewport: null }, url);
+    const { sourceHtml, page ,validation} = await dom.getDOM(browser, { viewport: null, execute:{ act: false, html: false, css: false, bp: false }, }, url);
     const evaluation = new Evaluation();
     const evaluationReport = await evaluation.evaluatePage(sourceHtml, page, { act: true, html: true, css: false, bp: false }, {}, url,validation);
     //console.log(evaluationReport);
