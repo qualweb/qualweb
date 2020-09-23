3cb206d6b09156e4fe90b44e251ae14600303bd9
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-09-23 11:51:09 +0100

    bug fixes

--- packages/act-rules/test/act-rules-all.spec.js
+++ packages/act-rules/test/act-rules-all.spec.js
@@ -9,11 +9,11 @@ const { Dom } = require('@qualweb/dom');
 
 describe('ACT-Rules module', function () {
   it('Should evaluate', async function () {
-    this.timeout(1000 * 10000);
+    this.timeout(1000 * 100000000);
     //['chromium', 'firefox', 'webkit']
     const browser = await playwright['chromium'].launch({ headless: false });
     const context = await browser.newContext({ bypassCSP: true });
-    const { sourceHtml, page, stylesheets } = await getDom(context, 'https://www.pcdiga.com/');//https://www.amazon.com/
+    const { sourceHtml, page, stylesheets } = await getDom(context,'https://mapa.eportugal.gov.pt/');//https://www.amazon.com/
     //https://observador.pt/
     /*
          const browser = await puppeteer.launch({headless:false});
