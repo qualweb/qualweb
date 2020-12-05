b3f9e8551f34bc7b8f334e9b4e4a6398d66c20ee
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-12-05 18:48:08 +0000

    updated dependencies

--- packages/evaluation/test/evaluation.spec.js
+++ packages/evaluation/test/evaluation.spec.js
@@ -6,17 +6,26 @@ describe('QualWeb page', function() {
   it('Testing qualweb page evaluation', async function() {
     this.timeout(60*1000);
 
-    const url = 'https://www.youtube.com/';
+    const url = 'http://www.appacdm-elvas.org ';
 
     const browser = await puppeteer.launch({headless:false});
     const dom = new Dom();
     const { sourceHtml, page ,validation} = await dom.getDOM(browser, {}, url);
     const evaluation = new Evaluation();
-    const evaluationReport = await evaluation.evaluatePage(sourceHtml, page, { act: false,  wcag: true, bp: false }, {}, url,validation);
-    //console.log(evaluationReport);
+    const evaluationReport = await evaluation.evaluatePage(sourceHtml, page, { act: true,  wcag: true, bp: true }, {}, url,validation);
+    const fs = require('fs')
+      // Write data in 'Output.txt' . 
+      fs.writeFile('Output.txt', JSON.stringify(evaluationReport.modules['wcag-techniques']['assertions'], null, 2), (err) => {
+        // In case of a error throw err. 
+        if (err) throw err;
+      })
+    console.log(evaluationReport.modules['wcag-techniques']['assertions']['QW-WCAG-T21']);
    // await dom.close();
    // await browser.close();
 
-    console.log(evaluationReport);
+    //console.log(evaluationReport);
   });
 });
+
+//http://www.apce.org.pt/
+//http://www.cm-caldas-rainha.pt/
