b94fdee6ccbd68d13937d59c17fbf355eb9cd151
aestriga <aestriga@lasige.di.fc.ul.pt>
2020-12-05 12:51:56 +0000

    new version

--- packages/qw-page/test/page.spec.js
+++ packages/qw-page/test/page.spec.js
@@ -1,19 +1,18 @@
-import playwright from 'playwright';
+const puppeteer = require('puppeteer');
 
 describe('QualWeb page', function() {
   it('Testing css rules mapping', async function() {
     this.timeout(1000 * 1000);
     //const browser = await playwright['chromium'].connect({ browserURL: 'http://127.0.0.1:9222/', defaultViewport: null });
 
-    const browser = await playwright.chromium.launch();
-    const context = await browser.newContext({ userAgent: 'chromium' });
-    const page = await context.newPage({
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage({
       bypassCSP: true
     });
 
-    await page.goto('https://www.pcdiga.com/', {
+    await page.goto('https://www.sapo.pt/', {
       timeout: 0,
-      waitUntil: 'networkidle'
+      waitUntil: 'networkidle2'
     });
 
     await page.addScriptTag({
@@ -22,11 +21,11 @@ describe('QualWeb page', function() {
 
     await page.evaluate(() => {
       // @ts-ignore
-      const page = new QWPage.QWPage(document, window, true);
+      window.page = new QWPage.QWPage(document, window, true);
     });
 
     await page.evaluate(() => {
-      console.log(page);
+      console.log(window.page);
       //new QWPage.QWPage(document, window, true);
     });
 
