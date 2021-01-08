02b0321d21dc8c1ad962e722417b6de604e8a2cd
João Vicente <joao.vicente@campus.ul.pt>
2021-01-08 15:51:46 +0000

    Updated dependencies and webpack config

--- packages/wcag-techniques/webpack.config.js
+++ packages/wcag-techniques/webpack.config.js
@@ -1,15 +1,28 @@
-const path = require('path')
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './dist/index.js',
-  node: {
-    net: true
-  },
   output: {
     filename: 'wcag.js',
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'var',
     library: 'WCAGTechniques'
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            keep_classnames: true
+          },
+          mangle: {
+            keep_classnames: true
+          }
+        }
+      })
+    ],
   },
   target: 'node-webkit',
 }
