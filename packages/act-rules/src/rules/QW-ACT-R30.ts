ef3be7a1487a6840e3105df274d3515e3e2b5705
João Vicente <joao.vicente@campus.ul.pt>
2021-02-01 14:19:16 +0000

    Updated R30

--- packages/act-rules/src/rules/QW-ACT-R30.ts
+++ packages/act-rules/src/rules/QW-ACT-R30.ts
@@ -2,20 +2,18 @@ import { ACTRuleResult } from '@qualweb/act-rules';
 import { AccessibilityUtils, DomUtils } from '@qualweb/util';
 import Rule from '../lib/Rule.object';
 import { ACTRuleDecorator, ElementExists } from '../lib/decorator';
-import {QWElement} from "@qualweb/qw-element";
-import {QWPage} from "@qualweb/qw-page";
-import LanguageDetect from "languagedetect";
+import { QWElement } from '@qualweb/qw-element';
+import { QWPage } from '@qualweb/qw-page';
+import LanguageDetect from 'languagedetect';
 
 @ACTRuleDecorator
 class QW_ACT_R30 extends Rule {
-
   constructor(rule?: any) {
     super(rule);
   }
 
   @ElementExists
   execute(element: QWElement, page: QWPage): void {
-
     const evaluation: ACTRuleResult = {
       verdict: '',
       description: '',
@@ -23,31 +21,39 @@ class QW_ACT_R30 extends Rule {
     };
 
     const isWidget = AccessibilityUtils.isElementWidget(element, page);
-    if(!isWidget) {
+    if (!isWidget) {
       evaluation.verdict = 'inapplicable';
       evaluation.description = `The test target is not a \`widget\`.`;
       evaluation.resultCode = 'RC1';
     } else {
       const supportsNameFromContent = AccessibilityUtils.allowsNameFromContent(element);
-      if(!supportsNameFromContent){
+      if (!supportsNameFromContent) {
         evaluation.verdict = 'inapplicable';
         evaluation.description = `The test target is not a \`widget\` that supports name from content.`;
         evaluation.resultCode = 'RC2';
       } else {
         const accessibleName = AccessibilityUtils.getAccessibleName(element, page);
-        const elementText = DomUtils.getTrimmedText(element,page);
+        const elementText = DomUtils.getTrimmedText(element, page);
         let hasTextNode = element.elementHasTextNode();
-        let isIconValue = this.isIcon(elementText,accessibleName,element);
+        let isIconValue = this.isIcon(elementText, accessibleName, element);
 
-        if(accessibleName === undefined) {
+        if (accessibleName === undefined) {
           evaluation.verdict = 'failed';
           evaluation.description = `The test target doesn't have an accessible name.`;
           evaluation.resultCode = 'RC6';
-        } else if(!hasTextNode || elementText === undefined || elementText === '' || elementText && !this.isHumanLanguage(elementText)&&!isIconValue) {
+        } else if (
+          !hasTextNode ||
+          elementText === undefined ||
+          elementText === '' ||
+          (elementText && !this.isHumanLanguage(elementText) && !isIconValue)
+        ) {
           evaluation.verdict = 'inapplicable';
           evaluation.description = `The test target has no visible text content or contains non-text content.`;
           evaluation.resultCode = 'RC3';
-        } else if(!!elementText &&(isIconValue ||accessibleName.toLowerCase().trim().includes(elementText.toLowerCase()))) {
+        } else if (
+          !!elementText &&
+          (isIconValue || accessibleName.toLowerCase().trim().includes(elementText.toLowerCase()))
+        ) {
           evaluation.verdict = 'passed';
           evaluation.description = `The complete visible text content of the test target either matches or is contained within its accessible name.`;
           evaluation.resultCode = 'RC4';
@@ -60,14 +66,13 @@ class QW_ACT_R30 extends Rule {
     }
     //console.log( evaluation.resultCode)
 
-    super.addEvaluationResult(evaluation, element,true,false,true,page);
-
+    super.addEvaluationResult(evaluation, element, true, false, true, page);
   }
-   //      let isIconValue = this.isIcon(elementText,accessibleName,element);
-   isIcon(elementText:string,accessibleName:string|undefined,element:QWElement): boolean {
-    const iconMap=['i','x'];
-    let fontStyle =element.getElementStyleProperty('font-family',null);
-    return !!accessibleName && (iconMap.includes(elementText.toLowerCase()) || fontStyle.includes("Material Icons"));
+  //      let isIconValue = this.isIcon(elementText,accessibleName,element);
+  isIcon(elementText: string, accessibleName: string | undefined, element: QWElement): boolean {
+    const iconMap = ['i', 'x'];
+    let fontStyle = element.getElementStyleProperty('font-family', null);
+    return !!accessibleName && (iconMap.includes(elementText.toLowerCase()) || fontStyle.includes('Material Icons'));
   }
 
   isHumanLanguage(string: string): boolean {
@@ -75,6 +80,5 @@ class QW_ACT_R30 extends Rule {
     return detector.detect(string).length > 0;
   }
 }
-
 
 export = QW_ACT_R30;
