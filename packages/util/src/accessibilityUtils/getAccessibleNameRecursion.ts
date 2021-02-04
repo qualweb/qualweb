81a36f59138075fef21ed96f0dc515fa3d2d2aa9
aestriga <aestriga@lasige.di.fc.ul.pt>
2021-02-04 16:27:48 +0000

    new version

--- packages/util/src/accessibilityUtils/getAccessibleNameRecursion.ts
+++ packages/util/src/accessibilityUtils/getAccessibleNameRecursion.ts
@@ -181,13 +181,10 @@ function getAccessibleNameFromAriaLabelledBy(
 }
 
 function getTextFromCss(element: QWElement, page: QWPage, isWidget: boolean): string {
-  let before = element.getElementStyleProperty('content', ':before');
-  let after = element.getElementStyleProperty('content', ':after');
+  let before = cleanSVGAndNoneCode(element.getElementStyleProperty('content', ':before'));
+  let after = cleanSVGAndNoneCode(element.getElementStyleProperty('content', ':after'));
   const aNameList = getAccessibleNameFromChildren(element, page, isWidget);
   const textValue = getConcatentedText(element, aNameList);
-
-  if (after === 'none') after = '';
-  if (before === 'none') before = '';
 
   return before.replace(/["']/g, '') + textValue + after.replace(/["']/g, '');
 }
@@ -197,6 +194,13 @@ function getConcatentedText(elementQW: QWElement, aNames: string[]): string {
     throw Error('Element is not defined');
   }
   return elementQW.concatANames(aNames);
+}
+
+function cleanSVGAndNoneCode(text: string): string {
+  if (!text || text === 'none'|| text.includes('url(')) {
+    text = ''
+  };
+  return text;
 }
 
 function getAccessibleNameFromChildren(element: QWElement, page: QWPage, isWidget: boolean): string[] {
