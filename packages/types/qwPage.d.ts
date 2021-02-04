affe5dca8e183737dd8f8f47c16f27d9f9ef8087
aestriga <aestriga@lasige.di.fc.ul.pt>
2021-02-04 17:39:13 +0000

    new version

--- packages/types/qwPage.d.ts
+++ packages/types/qwPage.d.ts
@@ -6,8 +6,8 @@ declare module '@qualweb/qw-page' {
     public cacheValue(selector: string, method: string, value: string | undefined): void;
     public getCachedValue(selector: string, method: string): string|undefined;
     public isValueCached(selector: string, method: string): boolean;
-    public getElement(selector: string, specificDocument?: QWElement): QWElement | null;
-    public getElements(selector: string, specificDocument?: QWElement): Array<QWElement>;
+    public getElement(selector: string, specificDocument?: QWElement,documentSelector?:string): QWElement | null;
+    public getElements(selector: string, specificDocument?: QWElement,documentSelector?:string): Array<QWElement>;
     public getElementByID(id: string): QWElement | null;
     public getElementByAttributeName(name: string): QWElement | null;
     public getPageRootElement(page: QWPage): QWElement | null;
