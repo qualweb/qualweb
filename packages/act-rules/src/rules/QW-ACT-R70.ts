72edb4b975721ad3193642cd29489bb0c219f47b
João Vicente <joao.vicente@campus.ul.pt>
2021-02-08 12:20:56 +0000

    Fixed known bugs. Updated dependencies

--- packages/act-rules/src/rules/QW-ACT-R70.ts
+++ packages/act-rules/src/rules/QW-ACT-R70.ts
@@ -4,12 +4,11 @@ import { ACTRuleResult } from '@qualweb/act-rules';
 import { AccessibilityUtils, DomUtils } from '@qualweb/util';
 import Rule from '../lib/Rule.object';
 import { ACTRuleDecorator, ElementExists, ElementHasNegativeTabIndex } from '../lib/decorator';
-import { QWElement } from "@qualweb/qw-element";
-import { QWPage } from "@qualweb/qw-page";
+import { QWElement } from '@qualweb/qw-element';
+import { QWPage } from '@qualweb/qw-page';
 
 @ACTRuleDecorator
 class QW_ACT_R70 extends Rule {
-
   constructor(rule?: any) {
     super(rule);
   }
@@ -25,30 +24,32 @@ class QW_ACT_R70 extends Rule {
 
     if (!DomUtils.isElementVisible(element, page)) {
       evaluation.verdict = 'passed';
-      evaluation.description = ' The nested browsing context does not include elements that are visible and part of the sequential focus navigation.';
+      evaluation.description =
+        ' The nested browsing context does not include elements that are visible and part of the sequential focus navigation.';
       evaluation.resultCode = 'RC1';
       super.addEvaluationResult(evaluation, element);
     } else {
-      let elementList = page.getElements("*", undefined, element.getElementSelector());
+      const elementList = page.getElements('*', undefined);
       let inSequentialFocusList = elementList.filter((elem) => {
-        return AccessibilityUtils.isPartOfSequentialFocusNavigation(elem, page) && DomUtils.isElementVisible(elem, page);
-      })
+        return (
+          AccessibilityUtils.isPartOfSequentialFocusNavigation(elem, page) && DomUtils.isElementVisible(elem, page)
+        );
+      });
 
       if (inSequentialFocusList.length === 0) {
         evaluation.verdict = 'passed';
-        evaluation.description = ' The nested browsing context does not include elements that are visible and part of the sequential focus navigation.';
+        evaluation.description =
+          ' The nested browsing context does not include elements that are visible and part of the sequential focus navigation.';
         evaluation.resultCode = 'RC1';
         super.addEvaluationResult(evaluation, element);
       } else {
         evaluation.verdict = 'failed';
-        evaluation.description = 'The nested browsing context includes elements that are visible and part of the sequential focus navigation.';
+        evaluation.description =
+          'The nested browsing context includes elements that are visible and part of the sequential focus navigation.';
         evaluation.resultCode = 'RC2';
         super.addEvaluationResult(evaluation, element, false, false);
       }
     }
-
-
-
   }
 }
 
