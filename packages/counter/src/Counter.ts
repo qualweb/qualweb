e523b61c748dcdabd3256bd6ceb41a8c2c9a5c15
PedroSPSantos <pedrosimaops@gmail.com>
2021-02-24 21:33:33 +0000

    Added Counter Report

--- packages/counter/src/Counter.ts
+++ packages/counter/src/Counter.ts
@@ -1,44 +1,41 @@
+import { QWPage } from "@qualweb/qw-page";
+import { AccessibilityUtils } from "@qualweb/util";
+import { CounterReport } from "@qualweb/counter";
 
-import { QWPage } from '@qualweb/qw-page';
-import {AccessibilityUtils} from  '@qualweb/util';
-import {CounterReport} from  '@qualweb/counter';
-async function executeCounter( page: QWPage ): Promise<CounterReport| void>{
+async function executeCounter(page: QWPage): Promise<CounterReport | void> {
+  let roles2: any = {}; //dicionario roles
+  let names: any = {}; //dicionario tag
+  const cr: CounterReport = {type: 'counter', data:{roles:{},tags:{}}};
+  const elementList = page.getElements("*");
 
-    let roles: any = {}; //dicionario roles
-    let names: any = {}; //dicionario tag
-    // const CounterReport = new CounterReport();
-    const elementList = page.getElements('*');
-  
   //get explicit roles
-  for(const element of elementList){
+  for (const element of elementList) {
     const role = AccessibilityUtils.getElementRole(element, page);
     const tag = element.getElementTagName();
     // count elements
-    if (role!== null){
+    if (role !== null) {
+      if (roles2[role] === undefined) {
+        roles2[role] = 0;
+      }
 
-    if(roles[role]=== undefined){
-      roles[role] = 0;
-    }
-
-    roles[role]++;
+      roles2[role]++;
     }
     // count tags
-    
-    if(tag!== null){
-      if(names[tag]=== undefined){
+
+    if (tag !== null) {
+      if (names[tag] === undefined) {
         names[tag] = 0;
       }
       names[tag]++;
-  }
-
     }
-
-console.log(roles, names);
-
   }
+  cr.data.roles = roles2;
+  cr.data.tags = names;
+  return cr;
+  
+}
 
-export{executeCounter};
-
+export { executeCounter };
     
 
   
