8e97ce7a74ceff6fee6aa713b23905e6756c938c
João Vicente <joao.vicente@campus.ul.pt>
2021-02-26 21:48:09 +0000

    Code optimized

--- packages/types/counter.d.ts
+++ packages/types/counter.d.ts
@@ -1,33 +1,25 @@
-declare module '@qualweb/counter' {
-  
-  import { QWPage } from '@qualweb/qw-page';  
+declare module "@qualweb/counter" {
+  import { QWPage } from "@qualweb/qw-page";
 
-  interface Roles{
+  interface Roles {
     [role: string]: number;
   }
 
-  interface Tags{
+  interface Tags {
     [tag: string]: number;
   }
 
   interface CounterResult {
-    roles: Roles ;
-    tags: Tags ;
+    roles: Roles;
+    tags: Tags;
   }
-
 
   interface CounterReport {
-    type: 'counter';
+    type: "counter";
     data: CounterResult;
   }
-  
-  function executeCounter( page: QWPage ): Promise<CounterReport>;
-  
-  export {
-    CounterResult,
-    CounterReport,
-    Roles,
-    Tags,
-    executeCounter,
-  };
+
+  function executeCounter(page: QWPage): Promise<CounterReport>;
+
+  export { CounterResult, CounterReport, Roles, Tags, executeCounter };
 }
