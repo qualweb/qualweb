242330277708ac74b5e2b1b70588b0fc4cdeee5a
João Vicente <joao.vicente@campus.ul.pt>
2021-02-27 16:58:19 +0000

    Fixed some bugs on evaluation.d.ts

--- packages/types/evaluation.d.ts
+++ packages/types/evaluation.d.ts
@@ -1,29 +1,54 @@
-declare module '@qualweb/evaluation' {
-  import { QualwebOptions, SourceHtml, EvaluationReport, Evaluator } from '@qualweb/core';
-  import { ACTRulesReport } from '@qualweb/act-rules';
-  import { HTMLTechniquesReport } from '@qualweb/html-techniques';
-  import { CSSTechniquesReport } from '@qualweb/css-techniques';
-  import { BestPracticesReport } from '@qualweb/best-practices';
-  import { Page } from 'puppeteer';
+declare module "@qualweb/evaluation" {
+  import {
+    QualwebOptions,
+    SourceHtml,
+    EvaluationReport,
+    Evaluator,
+    Execute,
+  } from "@qualweb/core";
+  import { ACTRulesReport } from "@qualweb/act-rules";
+  import { WCAGTechniquesReport, WCAGOptions } from "@qualweb/wcag-techniques";
+  import { BestPracticesReport } from "@qualweb/best-practices";
+  import { CounterReport } from "@qualweb/counter";
+  import { HTMLValidationReport } from "@qualweb/html-validator";
+  import { Page } from "puppeteer";
 
   class Evaluation {
-    public evaluatePage(sourceHtml: SourceHtml, page: Page, execute: any, options: QualwebOptions, url: string, validation: any): Promise<EvaluationRecord>;
+    public evaluatePage(
+      sourceHtml: SourceHtml,
+      page: Page,
+      execute: Execute,
+      options: QualwebOptions,
+      url: string,
+      validation: HTMLValidationReport
+    ): Promise<EvaluationRecord>;
     public addQWPage(page: Page): Promise<void>;
-    public executeBP(page: Page, options: QualwebOptions): Promise<BestPracticesReport>;
-    public executeCSS(page: Page, options: QualwebOptions): Promise<CSSTechniquesReport>;
-    public executeHTML(page: Page, options: QualwebOptions, validation: any): Promise<HTMLTechniquesReport>;
-    public executeACT(page: Page, sourceHtml: SourceHtml, options: QualwebOptions): Promise<ACTRulesReport>;
-    public getEvaluator(page: Page, sourceHtml: SourceHtml, url: string): Promise<Evaluator>;
+    public executeBP(
+      page: Page,
+      options: QualwebOptions
+    ): Promise<BestPracticesReport>;
+    public executeWCAG(
+      page: Page,
+      options: WCAGOptions | undefined,
+      validation: HTMLValidationReport
+    ): Promise<WCAGTechniquesReport>;
+    public executeACT(
+      page: Page,
+      sourceHtml: SourceHtml,
+      options: QualwebOptions
+    ): Promise<ACTRulesReport>;
+    public executeCounter(page: Page): Promise<CounterReport>;
+    public getEvaluator(
+      page: Page,
+      sourceHtml: SourceHtml,
+      url: string
+    ): Promise<Evaluator>;
   }
   class EvaluationRecord {
-
     constructor(evaluator: Evaluator);
     public addModuleEvaluation(module: any, evaluation: any): void;
     public getFinalReport(): EvaluationReport;
   }
 
-  export {
-    Evaluation,
-    EvaluationRecord
-  };
+  export { Evaluation, EvaluationRecord };
 }
