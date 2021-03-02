575052b96e60e27a6682977ee2f029617eaeaf74
João Vicente <joao.vicente@campus.ul.pt>
2021-03-02 12:19:59 +0000

    Fixed know bugs

--- packages/util/src/accessibilityUtils/elementHasValidRole.ts
+++ packages/util/src/accessibilityUtils/elementHasValidRole.ts
@@ -1,4 +1,3 @@
-'use strict';
 import { QWPage } from '@qualweb/qw-page';
 import { QWElement } from '@qualweb/qw-element';
 import roles from './roles.json';
@@ -7,7 +6,18 @@ import { AccessibilityUtils } from '@qualweb/util';
 function elementHasValidRole(elementQW: QWElement, pageQW: QWPage): boolean {
   const role = AccessibilityUtils.getElementRole(elementQW, pageQW);
   let result = false;
-  if (role !== null) result = Object.keys(roles).includes(role);
+  if (role !== null) {
+    if (role.trim().includes(' ')) {
+      for (const r of role.trim().split(' ')) {
+        result = Object.keys(roles).includes(r);
+        if (result) {
+          break;
+        }
+      }
+    } else {
+      result = Object.keys(roles).includes(role);
+    }
+  }
 
   return result;
 }
