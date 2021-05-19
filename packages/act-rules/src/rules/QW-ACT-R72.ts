b86f1d4e54b7258112adec530a3dab0d76dc6bc7
João Vicente <joao.vicente@campus.ul.pt>
2021-05-19 15:32:17 +0100

    Fixed QW-ACT-R72 bugs

--- packages/act-rules/src/rules/QW-ACT-R72.ts
+++ packages/act-rules/src/rules/QW-ACT-R72.ts
@@ -46,9 +46,10 @@ class QW_ACT_R72 extends AtomicRule {
         test.addElement(focused, false);
       } else if (focused?.getElementAttribute('href')) {
         const destination = focused.getElementAttribute('href')?.trim();
-        if (destination && destination.startsWith('#')) {
+        if (destination && this.checkDestination(destination)) {
           // only checking that it has an url that starts with # -- other ways of linking to the same page are not considered
-          if (window.qwPage.getElementByID(destination.substring(1))) {
+          console.log(destination.split('#')[1]);
+          if (window.qwPage.getElementByID(destination.split('#')[1])) {
             test.verdict = 'warning';
             test.description =
               'Check that the first focusable element has an accessible name that communicates that it skips content';
@@ -86,6 +87,14 @@ class QW_ACT_R72 extends AtomicRule {
     }
 
     super.addTestResult(test);
+  }
+
+  private checkDestination(destination: string): boolean {
+    const url = window.qwPage.getURL();
+    return destination.startsWith('#') || 
+      destination.startsWith('/#') || 
+      destination.startsWith(url + '#') || 
+      destination.startsWith(url + '/#')
   }
 }
 
