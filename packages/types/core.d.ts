a17cbae25bae7760cf5d08814344dd165d6c9a36
lindhart <johannes@the.homestead.dk>
2022-03-25 22:19:08 +0100

    feat: add initial plugin interfaces

--- packages/types/core.d.ts
+++ packages/types/core.d.ts
@@ -10,6 +10,7 @@ declare module "@qualweb/core" {
     LaunchOptions,
     BrowserLaunchArgumentOptions,
     BrowserConnectOptions,
+    Page,
     Viewport,
   } from "puppeteer";
   import { CounterReport } from "@qualweb/counter";
@@ -28,6 +29,33 @@ declare module "@qualweb/core" {
     | "domcontentloaded"
     | "networkidle0"
     | "networkidle2";
+
+
+  type PageCallback = (page: Page) => Promise<void> | void;
+
+  /**
+   * Basic interface for a Qualweb plugin. Plugins are useful when you need to
+   * work on a page before letting Qualweb perform evaluation of its contents.
+   */
+  export interface QualwebPlugin {
+    /**
+     * Called *after* a Page object has been initialized but *before* it loads
+     * a target URL and injects Qualweb.
+     * Use cases include injecting cookies before page loads, setting the
+     * user agent prior to loading the target URL, and even subscribing to
+     * console events for diagnostic/logging purposes.
+     */
+    beforePageLoad?: PageCallback;
+
+    /**
+     * Called *after* navigating to a URL that should be evaluated, but before
+     * the evaluation itself has been run.
+     * Use cases include dismissing banners/popups and adding timer delays to
+     * slow-loading pages, to guarantee content has loading prior to Qualweb's
+     * evaluation.
+     */
+    afterPageLoad?: PageCallback;
+  }
 
   interface QualwebOptions {
     url?: string;
@@ -173,6 +201,8 @@ declare module "@qualweb/core" {
         BrowserLaunchArgumentOptions &
         BrowserConnectOptions
     ): Promise<void>;
+
+    public use(plugin: QualwebPlugin): this;
 
     /**
      * Closes chromium browser.
