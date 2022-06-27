f97245aaff8383f9ecc094f6cb6e426eccc4a419
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-06-27 18:48:46 +0100

    bug fix

--- packages/util/src/domUtils/isElementHiddenByCSSAux.ts
+++ packages/util/src/domUtils/isElementHiddenByCSSAux.ts
@@ -3,7 +3,6 @@ function isElementHiddenByCSSAux(element: typeof window.qwElement): boolean {
   const displayNone = display ? display.trim() === 'none' : false;
   const visibilityATT = element.getElementStyleProperty('visibility', '');
   const visibility = visibilityATT ? visibilityATT.trim() === 'collapse' || visibilityATT.trim() === 'hidden' : false;
-
   return visibility || displayNone;
 }
 
