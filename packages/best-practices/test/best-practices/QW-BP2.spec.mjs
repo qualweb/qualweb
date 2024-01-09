7834287e7b5c0c49099c64c5338644457ae91f8e
lindhart <johannes@the.homestead.dk>
2024-01-09 23:58:22 +0100

    test(best-practices): fixed up QW-BP2 tests
    The tests fail, and I *think* it's a genuine error in test code and not the unit test, but I need someone else to confirm for me.
    
    The changes in this file serve as guidance for how to fix the issues with all other tests for this package.

--- packages/best-practices/test/best-practices/QW-BP2.spec.mjs
+++ packages/best-practices/test/best-practices/QW-BP2.spec.mjs
@@ -1,63 +1,68 @@
-const { BestPractices } = require('../../dist/index');
-const { expect } = require('chai');
-const puppeteer = require('puppeteer');
-const { getDom } = require('../getDom');
+import { expect } from 'chai';
+import fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
+import locales from '@qualweb/locale';
 import { createRequire } from 'module';
+import { usePuppeteer } from '../util.mjs';
 const require = createRequire(import.meta.url);
 
-describe('Best practice QW-BP2', function () {
+describe('QW-BP2', function () {
   const tests = [
     {
-      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~aestriga/TesteBP-2/test1.html',
+      path: '../fixtures/aestriga/TesteBP-2/test1.html',
       outcome: 'passed'
     },
     {
-      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~aestriga/TesteBP-2/test2.html',
+      path: '../fixtures/aestriga/TesteBP-2/test2.html',
       outcome: 'failed'
     },
     {
-      url: 'http://accessible-serv.lasige.di.fc.ul.pt/~aestriga/TesteBP-2/test3.html',
+      path: '../fixtures/aestriga/TesteBP-2/test3.html',
       outcome: 'inapplicable'
     }
   ];
-  let browser;
-  it('pup open', async function () {
-    browser = await puppeteer.launch();
-  });
-  let i = 0;
-  let lastOutcome = 'warning';
-  for (const test of tests || []) {
-    if (test.outcome !== lastOutcome) {
-      lastOutcome = test.outcome;
-      i = 0;
-    }
-    i++;
-    describe(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, function () {
-      it(`should have outcome="${test.outcome}"`, async function () {
-        this.timeout(10 * 1000);
-        const { sourceHtml, page, stylesheets } = await getDom(browser, test.url);
-        await page.addScriptTag({
-          path: require.resolve('@qualweb/qw-page').replace('index.js', 'qwPage.js')
-        });
-        await page.addScriptTag({
-          path: require.resolve('../../dist/bp.js')
-        });
-        const report = await page.evaluate(
-          (rules) => {
-            const bp = new BestPractices.BestPractices(rules);
-            let report = bp.execute(new QWPage.QWPage(document, window));
-            return report;
-          },
-          { bestPractices: ['QW-BP2'] }
-        );
 
-        expect(report['assertions']['QW-BP2'].metadata.outcome).to.be.equal(test.outcome);
+  const proxy = usePuppeteer();
+
+  for (const test of tests || []) {
+    it(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)}`, async function () {
+      this.timeout(10 * 1000);
+      
+      const page = proxy.page;
+
+      const targetUrl = test.path
+        ? path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), test.path)
+        : test.url
+        ;
+
+      // console.log(`Using test case from "${targetUrl}"`);
+
+      await page.goto(url.pathToFileURL(targetUrl), {
+        waitUntil: [
+          'networkidle2',
+          'domcontentloaded',
+        ],
       });
+
+      await page.addScriptTag({
+        path: require.resolve('@qualweb/qw-page')
+      });
+
+      await page.addScriptTag({
+        path: require.resolve('../../dist/bp.bundle.js')
+      });
+
+      const report = await page.evaluate(
+        (rules, locales) => {
+          const bp = new BestPractices(JSON.parse(locales), rules);
+          let report = bp.execute(new QWPage(document, window));
+          return report;
+        },
+        { bestPractices: ['QW-BP2'] }, JSON.stringify(locales.default.en),
+      );
+
+      expect(report['assertions']['QW-BP2'].metadata.outcome).to.be.equal(test.outcome);
     });
   }
-  describe(``, function () {
-    it(`pup shutdown`, async function () {
-      await browser.close();
-    });
-  });
 });
