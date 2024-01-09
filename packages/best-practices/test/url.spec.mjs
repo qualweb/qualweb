5ca67e266911c7765e5b6b6272547822ff069900
Johannes L. Borresen <jlb@inqludeit.dk>
2024-01-09 16:20:42 +0100

    test(best-practices): clean up url.spec.mjs

--- packages/best-practices/test/url.spec.mjs
+++ packages/best-practices/test/url.spec.mjs
@@ -3,45 +3,39 @@ import puppeteer from 'puppeteer';
 import { Dom } from '@qualweb/dom';
 import locales from '@qualweb/locale';
 import { createRequire } from 'module';
+import { usePuppeteer } from './util.mjs';
 const require = createRequire(import.meta.url);
 
-describe('Running tests', function () {
+describe('url.spec.mjs', function () {
+  const proxy = usePuppeteer();
+
   it('Evaluates url', async function () {
     this.timeout(0);
 
     const url = 'https://www.ccb.pt/';
 
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: ['--ignore-certificate-errors']
-    });
-    const incognito = await browser.createIncognitoBrowserContext();
-    const page = await incognito.newPage();
-    const dom = new Dom(page);
+    const dom = new Dom(proxy.page);
     await dom.process({ execute: { bp: false } }, url, '');
 
-    await page.addScriptTag({
+    await proxy.page.addScriptTag({
       path: require.resolve('@qualweb/qw-page')
     });
 
-    await page.addScriptTag({
+    await proxy.page.addScriptTag({
       path: require.resolve('@qualweb/util')
     });
 
-    await page.addScriptTag({
+    await proxy.page.addScriptTag({
       path: require.resolve('../dist/bp.bundle.js')
     });
 
-    const report = await page.evaluate((locale) => {
+    const report = await proxy.page.evaluate((locale) => {
       const bp = new BestPractices({ translate: locale, fallback: locale }, { bestPractices: ['QW-BP18'] });
       return bp.execute();
     }, locales.default.en);
 
-    /*await page.close();
-    await incognito.close();
-    await browser.close();*/
+    // console.log(JSON.stringify(report, null, 2));
 
-    console.log(JSON.stringify(report, null, 2));
     expect(report);
   });
 });
