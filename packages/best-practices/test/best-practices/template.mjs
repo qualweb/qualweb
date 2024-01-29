2b648b391f5cfe1d4bfd00704cbba0d1e5b2549c
Johannes L. Borresen <jlb@inqludeit.dk>
2024-01-29 14:38:48 +0100

    test(best-practices): improve test builder `buildTest()`
    - Changed the call signature. The function now expects the path to a testcases.json file that defines the tests, instead of the raw array.
    - Added support for HTML files in addition to URLs. If both are present, the local file is preferred.
    - Fixed test cases having duplicate identifiers.

--- packages/best-practices/test/best-practices/template.mjs
+++ packages/best-practices/test/best-practices/template.mjs
@@ -1,3 +1,6 @@
+import fs from 'node:fs';
+import path from 'path';
+
 import { expect } from 'chai';
 import { Dom } from '@qualweb/dom';
 import { createRequire } from 'module';
@@ -12,22 +15,44 @@ const require = createRequire(import.meta.url);
  * a test case just needs to load up puppeteer, inject BP files, run a test,
  * and expect a specific outcome.
  * @param {string} bestpracticeName Name of the best practice, like "QW-BP2".
- * @param {{ url: string, path: string, outcome: string }[]} testcases Testcases to run for the BP rule.
+ * @param {string} testcasesPath Path to the testcases.json file that contains
+ * all the test cases. It will be treated as the base path for any file paths
+ * within the file.
  */
-export function buildTest(bestpracticeName, testcases) {
+export function buildTest(bestpracticeName, testcasesPath) {
   // Uppercase the rule name, just in case.
   bestpracticeName = bestpracticeName.toUpperCase();
+
+  const testcases = JSON.parse(fs.readFileSync(testcasesPath));
 
   const proxy = usePuppeteer();
 
   describe(bestpracticeName, () => {
-    for (const test of testcases || []) {
+    // Simple map to help count the occurrences of outcomes. It is used to
+    // ensure unique test case names (so two "inapplicable" tests can exist,
+    // with their own number).
+    const outcomeCounters = {};
+
+    for (const test of testcases) {
+      if (outcomeCounters[test.outcome]) {
+        outcomeCounters[test.outcome] += 1;
+      } else {
+        outcomeCounters[test.outcome] = 1;
+      }
+
       // it(`${test.outcome.charAt(0).toUpperCase() + test.outcome.slice(1)} example ${i}`, async function () {
-      it(test.outcome, async function () {
+      it(`${test.outcome} ${outcomeCounters[test.outcome]}`, async function () {
         this.timeout(0);
 
         const dom = new Dom(proxy.page);
-        await dom.process({ execute: { bp: true } }, test.url, null);
+
+        if (test.path) {
+          const testfilePath = path.resolve(path.dirname(testcasesPath), test.path);
+
+          await dom.process({ execute: { bp: true } }, null, fs.readFileSync(testfilePath, 'utf-8'));
+        } else if (test.url) {
+          await dom.process({ execute: { bp: true } }, test.url, null);
+        }
 
         await proxy.page.addScriptTag({
           path: require.resolve('@qualweb/qw-page')
