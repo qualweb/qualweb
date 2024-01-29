691379966ed26799be399ce7b431e55d3ed42535
Johannes L. Borresen <jlb@inqludeit.dk>
2024-01-29 13:46:13 +0100

    test(best-practices): refactored BP1 test to fit with new placement of testcases filse

--- packages/best-practices/test/best-practices/QW-BP1.spec.mjs
+++ packages/best-practices/test/best-practices/QW-BP1.spec.mjs
@@ -1,7 +1,5 @@
 import { buildTest } from './template.mjs';
 
-import allTestcases from '../fixtures/testcases.json' assert { type: 'json' };
+import testcases from '../fixtures/testcases/H42/testcases.json' assert { type: 'json' };
 
-const bp1Testcase = allTestcases.testcases['qw-bp1'];
-
-buildTest(bp1Testcase.name, bp1Testcase.testcases);
+buildTest('QW-BP1', testcases);
