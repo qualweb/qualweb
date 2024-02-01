77c020844c4b8c1b3cabd3643915f7a09673e4ff
Johannes L. Borresen <jlb@inqludeit.dk>
2024-02-01 15:08:51 +0100

    test(dom): get unit tests running again, use local file instead of remote server for web page
    One of the tests still fails. It looks like it was improperly implemented.

--- packages/dom/test/dom.spec.mjs
+++ packages/dom/test/dom.spec.mjs
@@ -1,33 +1,54 @@
-import { Dom } from '../dist/index.js';
-import puppeteer from 'puppeteer';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import { expect } from 'chai';
 
-describe('HTML validation', function () {
-  it('should be undefined', async function () {
-    this.timeout(0);
-    const browser = await puppeteer.launch({ headless: true });
-    const incognito = await browser.createIncognitoBrowserContext();
-    const page = await incognito.newPage();
-    const dom = new Dom(page);
-    const { validation } = await dom.process({}, 'https://ciencias.ulisboa.pt', '');
+import { Dom } from '../dist/index.js';
+import puppeteer from 'puppeteer';
 
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+describe('dom.process()', () => {
+  let browser;
+  let incognito;
+  let page;
+
+  beforeEach(async () => {
+    browser = await puppeteer.launch({
+      headless: 'new',
+    });
+    incognito = await browser.createIncognitoBrowserContext();
+    page = await incognito.newPage();
+  });
+
+  afterEach(async () => {
     await page.close();
     await incognito.close();
     await browser.close();
+  })
+
+  it('No ACT/WCAG execution intent should not yield validation', async function () {
+    this.timeout(0);
+    
+    const emptyHtmlFile = readFileSync(resolve(__dirname, 'fixtures/empty.html'));
+
+    const dom = new Dom(page);
+
+    const { validation } = await dom.process({}, '', emptyHtmlFile);
 
     expect(validation).to.be.undefined;
   });
-  it('should not be undefined', async function () {
+  it('ACT/WCAG execution intent must yield validation', async function () {
     this.timeout(0);
-    const browser = await puppeteer.launch({ headless: true });
-    const incognito = await browser.createIncognitoBrowserContext();
-    const page = await incognito.newPage();
-    const dom = new Dom(page /** INSERT ENDPOINT HERE **/);
-    const { validation } = await dom.process({ execute: { wcag: true } }, 'https://ciencias.ulisboa.pt', '');
 
-    await page.close();
-    await incognito.close();
-    await browser.close();
+    const emptyHtmlFile = readFileSync(resolve(__dirname, 'fixtures/empty.html'));
+
+    const dom = new Dom(page, /** MISSING VALIDATOR HERE! */);
+
+    const { validation } = await dom.process({ execute: { wcag: true } }, '', emptyHtmlFile);
+
+    console.debug(validation);
 
     expect(validation).to.not.be.undefined;
   });
