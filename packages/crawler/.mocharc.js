6e2496e5ed2632409db3e367ddda2788a215201a
Johannes L. Borresen <jlb@inqludeit.dk>
2024-02-01 13:37:01 +0100

    test(crawler): removing old test files, adding mocharc file

--- packages/crawler/.mocharc.js
+++ packages/crawler/.mocharc.js
@@ -2,4 +2,9 @@ module.exports = {
   "color": true,
   "ui": "bdd",
   "spec": "test/**/*.spec.mjs",
+  // Uncomment to get a JSON file with test results instead of output in console.
+  // "reporter": "json",
+  // "reporterOptions": [
+  //   "output=./results.json",
+  // ],
 }
