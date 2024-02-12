89fc37d4f51d8e1be5639bd30098e77f9687dff5
Johannes L. Borresen <jlb@inqludeit.dk>
2024-02-12 14:06:39 +0100

    test(core): migrate log.spec.mjs to TS

--- packages/core/test/log.spec.ts
+++ packages/core/test/log.spec.ts
@@ -1,16 +1,45 @@
 import { QualWeb } from '../dist/index.js';
 import { expect } from 'chai';
 
+import { createKoaServer } from './util';
+import { Server } from 'http';
+
 describe('Log', function () {
+  // Mock server set up.
+  const mockServerOptions = {
+    childLinksPerPage: 3,
+    maxDepth: 3,
+  };
+
+  const mockServer = createKoaServer(mockServerOptions);
+
+  let mockHttpServer: Server;
+  let mockHttpServerHost: string;
+
+  before(async () => {
+    await new Promise(r => mockHttpServer = mockServer.listen(r));
+
+    const address = mockHttpServer.address();
+
+    mockHttpServerHost = typeof(address) === 'string'
+      ? address
+      : `http://localhost:${address!.port}`
+      ;
+  });
+
+  after(async () => {
+    mockHttpServer.close();
+  });
+
   it('Should log to file', async function () {
     this.timeout(0);
 
     const qualweb = new QualWeb({ adBlock: true, stealth: true });
 
-    await qualweb.start({ headless: true, args: ['--ignore-certificate-errors'] });
+    await qualweb.start(undefined, { headless: 'new', args: ['--ignore-certificate-errors'] });
 
     const evaluations = await qualweb.evaluate({
-      url: 'http://localhost:4200/',
+      url: mockHttpServerHost,
       execute: { act: true, wcag: true, bp: true },
       waitUntil: ['load', 'networkidle0'],
       log: {
@@ -20,17 +49,17 @@ describe('Log', function () {
 
     await qualweb.stop();
 
-    expect(evaluations['http://localhost:4200/']).to.not.be.undefined;
+    expect(evaluations[mockHttpServerHost]).to.not.be.undefined;
   });
   it('Should log to console', async function () {
     this.timeout(0);
 
     const qualweb = new QualWeb({ adBlock: true, stealth: true });
 
-    await qualweb.start({ headless: true, args: ['--ignore-certificate-errors'], monitor: true });
+    await qualweb.start({ monitor: true }, { headless: 'new', args: ['--ignore-certificate-errors'] });
 
     const evaluations = await qualweb.evaluate({
-      url: 'http://localhost:4200/',
+      url: mockHttpServerHost,
       execute: { act: true, wcag: true, bp: true },
       waitUntil: ['load', 'networkidle0'],
       log: {
@@ -40,18 +69,18 @@ describe('Log', function () {
 
     await qualweb.stop();
 
-    expect(evaluations['http://localhost:4200/']).to.not.be.undefined;
+    expect(evaluations[mockHttpServerHost]).to.not.be.undefined;
   });
   it('Should log to console 2', async function () {
     this.timeout(0);
 
     const qualweb = new QualWeb({ adBlock: true, stealth: true });
 
-    await qualweb.start({ headless: true, args: ['--ignore-certificate-errors'], monitor: true });
+    await qualweb.start({ monitor: true }, { headless: 'new', args: ['--ignore-certificate-errors'] });
 
     const evaluations = await qualweb.evaluate({
       urls: [
-        'http://localhost:4200/',
+        mockHttpServerHost,
         'http://localhost:4200/1',
         'http://localhost:4200/2',
         'http://localhost:4200/3',
@@ -67,6 +96,6 @@ describe('Log', function () {
 
     await qualweb.stop();
 
-    expect(evaluations['http://localhost:4200/']).to.not.be.undefined;
+    expect(evaluations[mockHttpServerHost]).to.not.be.undefined;
   });
 });
