50f249b80cb8523c3b3195910c420fdd76df05c0
lindhart <johannes@the.homestead.dk>
2024-10-10 15:36:19 +0200

    test(core): re-enable disabled unit tests, rename .mjs test to .ts

--- packages/core/test/html.spec.ts
+++ packages/core/test/html.spec.ts
@@ -1,21 +1,21 @@
-// import { QualWeb } from '../src';
-// import { expect } from 'chai';
-// import { readFileSync } from 'fs';
+import { QualWeb } from '../src';
+import { expect } from 'chai';
+import { readFileSync } from 'fs';
 
-// describe('Core input method: html', function () {
-//   it('Should evaluate normally', async function () {
-//     this.timeout(0);
+describe('Core input method: html', function () {
+  it('Should evaluate normally', async function () {
+    this.timeout(0);
 
-//     const options = {
-//       html: readFileSync('./test/test.html').toString()
-//     };
+    const options = {
+      html: readFileSync('./test/test.html').toString()
+    };
 
-//     const qualweb = new QualWeb();
+    const qualweb = new QualWeb();
 
-//     await qualweb.start();
-//     const reports = await qualweb.evaluate(options);
-//     await qualweb.stop();
+    await qualweb.start();
+    const reports = await qualweb.evaluate(options);
+    await qualweb.stop();
 
-//     expect(reports['customHtml']).to.not.be.equal(undefined);
-//   });
-// });
+    expect(reports['customHtml']).to.not.be.equal(undefined);
+  });
+});
--- packages/core/test/plugin.spec.ts
+++ packages/core/test/plugin.spec.ts
@@ -1,241 +1,240 @@
-// import { QualWeb } from '../src';
-// import { expect } from 'chai';
-// import { readFileSync } from 'fs';
-// import { Protocol } from 'puppeteer';
+import { QualWeb } from '../src';
+import { expect } from 'chai';
+import { readFileSync } from 'fs';
+import { Cookie } from 'puppeteer';
 
-// describe('Plugins', function () {
-//   // All tests seem to be somewhat time sensitive. 10 seconds *should* be enough
-//   // for any one test on most setups.
-//   this.timeout(10000);
+describe('Plugins', function () {
+  // All tests seem to be somewhat time sensitive. 10 seconds *should* be enough
+  // for any one test on most setups.
+  this.timeout(10000);
 
-//   // Re-usable "execute nothing" options for calls to evaluate()
-//   const executeOptions = {
-//     wappalyzer: false,
-//     act: false,
-//     wcag: false,
-//     bp: false,
-//     counter: false,
-//   }
+  // Re-usable "execute nothing" options for calls to evaluate()
+  const executeOptions = {
+    "act-rules": false,
+    "wcag-techniques": false,
+    "best-practices": false,
+    counter: false,
+  }
 
-//   // Load up the html file prior to running the tests. Saves on I/O time.
-//   let html: string;
-//   before(() => {
-//     html = readFileSync('./test/test.html').toString();
-//   });
+  // Load up the html file prior to running the tests. Saves on I/O time.
+  let html: string;
+  before(() => {
+    html = readFileSync('./test/test.html').toString();
+  });
 
-//   // Initialize a new QualWeb instance for every test. Important, since there's
-//   // no mechanism to remove a plugin once added (by design).
+  // Initialize a new QualWeb instance for every test. Important, since there's
+  // no mechanism to remove a plugin once added (by design).
 
-//   let qualweb: QualWeb;
+  let qualweb: QualWeb;
 
-//   beforeEach(async () => {
-//     qualweb = new QualWeb({ });
-//     await qualweb.start(undefined, { headless: 'new' });
-//   });
+  beforeEach(async () => {
+    qualweb = new QualWeb({ });
+    await qualweb.start(undefined, { headless: true });
+  });
   
-//   afterEach(async () => {
-//     await qualweb.stop();
-//     qualweb = null as any;
-//   });
+  afterEach(async () => {
+    await qualweb.stop();
+    qualweb = null as any;
+  });
 
-//   it('Should run beforePageLoad for a plugin', async function () {
-//     const plugin = {
-//       pluginWasCalled: false,
+  it('Should run beforePageLoad for a plugin', async function () {
+    const plugin = {
+      pluginWasCalled: false,
 
-//       async beforePageLoad() {
-//         this.pluginWasCalled = true;
-//       }
-//     }
+      async beforePageLoad() {
+        this.pluginWasCalled = true;
+      }
+    }
 
-//     qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(plugin.pluginWasCalled).to.be.true;
-//   });
+    expect(plugin.pluginWasCalled).to.be.true;
+  });
 
-//   it('Should run afterPageLoad for a plugin', async function () {
-//     const plugin = {
-//       pluginWasCalled: false,
+  it('Should run afterPageLoad for a plugin', async function () {
+    const plugin = {
+      pluginWasCalled: false,
 
-//       async afterPageLoad() {
-//         this.pluginWasCalled = true;
-//       }
-//     }
+      async afterPageLoad() {
+        this.pluginWasCalled = true;
+      }
+    }
 
-//     qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     qualweb.stop();
+    qualweb.stop();
 
-//     expect(plugin.pluginWasCalled).to.be.true;
-//   });
+    expect(plugin.pluginWasCalled).to.be.true;
+  });
 
-//   it('Should run beforePageLoad once for each plugin that was added', async function () {
-//     let pluginCallCount = 0;
+  it('Should run beforePageLoad once for each plugin that was added', async function () {
+    let pluginCallCount = 0;
 
-//     const plugin = {
-//       async beforePageLoad() {
-//         pluginCallCount++;
-//       }
-//     };
+    const plugin = {
+      async beforePageLoad() {
+        pluginCallCount++;
+      }
+    };
 
-//     // Three uses. The fact that it's the same plugin doesn't matter
-//     qualweb.use(plugin);
-//     qualweb.use(plugin);
-//     qualweb.use(plugin);
+    // Three uses. The fact that it's the same plugin doesn't matter
+    qualweb.use(plugin);
+    qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(pluginCallCount).to.equal(3);
-//   });
+    expect(pluginCallCount).to.equal(3);
+  });
 
-//   it('Should run afterPageLoad once for each plugin that was added', async function () {
-//     let pluginCallCount = 0;
+  it('Should run afterPageLoad once for each plugin that was added', async function () {
+    let pluginCallCount = 0;
 
-//     const plugin = {
-//       async afterPageLoad() {
-//         pluginCallCount++;
-//       }
-//     }
+    const plugin = {
+      async afterPageLoad() {
+        pluginCallCount++;
+      }
+    }
 
-//     // Three uses. The fact that it's the same plugin doesn't matter
-//     qualweb.use(plugin);
-//     qualweb.use(plugin);
-//     qualweb.use(plugin);
+    // Three uses. The fact that it's the same plugin doesn't matter
+    qualweb.use(plugin);
+    qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(pluginCallCount).to.equal(3);
-//   });
+    expect(pluginCallCount).to.equal(3);
+  });
 
-//   it('Should run both beforePageLoad and afterPageLoad in the same plugin', async function () {
-//     let beforeWasCalled = false;
-//     let afterWasCalled = false;
+  it('Should run both beforePageLoad and afterPageLoad in the same plugin', async function () {
+    let beforeWasCalled = false;
+    let afterWasCalled = false;
 
-//     qualweb.use({
-//       beforePageLoad() {
-//         beforeWasCalled = true;
-//       },
-//       afterPageLoad() {
-//         afterWasCalled = true;
-//       }
-//     });
+    qualweb.use({
+      beforePageLoad() {
+        beforeWasCalled = true;
+      },
+      afterPageLoad() {
+        afterWasCalled = true;
+      }
+    });
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(beforeWasCalled).to.be.true;
-//     expect(afterWasCalled).to.be.true;
-//   });
+    expect(beforeWasCalled).to.be.true;
+    expect(afterWasCalled).to.be.true;
+  });
 
-//   it('Should pass target URL to a plugin (single URL)', async function () {
-//     const targetUrl = 'https://www.github.com';
-//     let reportedUrl;
+  it('Should pass target URL to a plugin (single URL)', async function () {
+    const targetUrl = 'https://www.github.com';
+    let reportedUrl;
 
-//     const plugin = {
-//       async beforePageLoad(_: unknown, url: string) {
-//         reportedUrl = url;
-//       }
-//     };
+    const plugin = {
+      async beforePageLoad(_: unknown, url: string) {
+        reportedUrl = url;
+      }
+    };
 
-//     qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       url: targetUrl,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      url: targetUrl,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(reportedUrl).to.be.a('string').and.equal(targetUrl);
-//   });
+    expect(reportedUrl).to.be.a('string').and.equal(targetUrl);
+  });
 
-//   it('Should pass target URLs to a plugin', async function () {
-//     const targetUrls: string[] = [
-//       'https://ciencias.ulisboa.pt',
-//       'https://www.github.com',
-//     ];
+  it('Should pass target URLs to a plugin', async function () {
+    const targetUrls: string[] = [
+      'https://ciencias.ulisboa.pt',
+      'https://www.github.com',
+    ];
 
-//     let reportedUrls: string[] = [];
+    let reportedUrls: string[] = [];
 
-//     const plugin = {
-//       async beforePageLoad(_: unknown, url: string) {
-//         reportedUrls.push(url);
-//       }
-//     }
+    const plugin = {
+      async beforePageLoad(_: unknown, url: string) {
+        reportedUrls.push(url);
+      }
+    }
 
-//     qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       urls: targetUrls,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      urls: targetUrls,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(reportedUrls).to.have.members(targetUrls);
-//   });
+    expect(reportedUrls).to.have.members(targetUrls);
+  });
 
-//   it('Should pass "customHtml" as URL to the plugin (when raw HTML is passed)', async function () {
-//     let reportedUrl;
+  it('Should pass "customHtml" as URL to the plugin (when raw HTML is passed)', async function () {
+    let reportedUrl;
 
-//     const plugin = {
-//       async beforePageLoad(_: unknown, url: string) {
-//         reportedUrl = url;
-//       }
-//     }
+    const plugin = {
+      async beforePageLoad(_: unknown, url: string) {
+        reportedUrl = url;
+      }
+    }
 
-//     qualweb.use(plugin);
+    qualweb.use(plugin);
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(reportedUrl).to.be.a('string').and.equal('customHtml');
-//   });
+    expect(reportedUrl).to.be.a('string').and.equal('customHtml');
+  });
 
-//   it('A cookie that was injected prior to page load should be visble after page load', async function () {
-//     let cookies: Protocol.Network.Cookie[] = [];
+  it('A cookie that was injected prior to page load should be visble after page load', async function () {
+    let cookies: Cookie[] = [];
 
-//     const testCookie = {
-//       name: 'OurTestCookie',
-//       value: 'true',
-//       url: 'http://localhost.null', // So we can retrieve it later.
-//       domain: 'localhost.null',
-//     };
+    const testCookie = {
+      name: 'OurTestCookie',
+      value: 'true',
+      url: 'http://localhost.null', // So we can retrieve it later.
+      domain: 'localhost.null',
+    };
 
-//     qualweb.use({
-//       beforePageLoad: async (page) => {
-//         // Inject a simple cookie.
-//         await page.setCookie(testCookie);
-//       },
-//       afterPageLoad: async (page) => {
-//         // Retrieve the cookies for our test URL.
-//         cookies = await page.cookies(testCookie.url);
-//       },
-//     });
+    qualweb.use({
+      beforePageLoad: async (page) => {
+        // Inject a simple cookie.
+        await page.setCookie(testCookie);
+      },
+      afterPageLoad: async (page) => {
+        // Retrieve the cookies for our test URL.
+        cookies = await page.cookies(testCookie.url);
+      },
+    });
 
-//     await qualweb.evaluate({
-//       html,
-//       modulesToExecute: executeOptions,
-//     });
+    await qualweb.evaluate({
+      html,
+      modulesToExecute: executeOptions,
+    });
 
-//     expect(cookies).to.be.an('array').and.have.length.at.least(1);
-//     expect(cookies[0].domain).to.equal(testCookie.domain);
-//     expect(cookies[0].name).to.equal(testCookie.name);
-//     expect(cookies[0].value).to.equal(testCookie.value);
-//   });
-// });
+    expect(cookies).to.be.an('array').and.have.length.at.least(1);
+    expect(cookies[0].domain).to.equal(testCookie.domain);
+    expect(cookies[0].name).to.equal(testCookie.name);
+    expect(cookies[0].value).to.equal(testCookie.value);
+  });
+});
