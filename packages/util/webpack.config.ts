f555f3fb0cb0f95700dd78497d38e95a5faa6561
lindhart <johannes@the.homestead.dk>
2024-10-20 16:38:51 +0200

    build(util): add all build output to distributed files
    The overarching change is to add the regular build output as exported components, so it can be imported piece-meal by other packages instead of the barrel file.
    
    - tsconfig updated to output to dist instead of prebuild, signifying that the entire build output is to be bundled in the NPM package.
    - webpack configuration has been updated to place the bundle in dist/__webpack, separated from the rest of the distributed contents.
    - package.json has been updated so "main" still points to the bundled JS file. *In addition*, an exports field has been added in place of "types" that exports the primary components of util for use in Node-based packages.

--- packages/util/webpack.config.ts
+++ packages/util/webpack.config.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
 
 export const mode = 'production';
-export const entry = './prebuild/index.js';
+export const entry = './dist/index.js';
 export const output = {
   filename: 'util.bundle.js',
-  path: path.resolve(__dirname, 'dist'),
+  path: path.resolve(__dirname, 'dist/__webpack'),
   library: {
     type: 'this'
   }
