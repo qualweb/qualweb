f9d906562e6df374efeb2938ee95989c5815c86a
lindhart <johannes@the.homestead.dk>
2024-10-26 20:44:42 +0200

    refactor(best-practices): refactor to properly inherit new definition of `EvaluationModuleDefinition`

--- packages/best-practices/src/index.ts
+++ packages/best-practices/src/index.ts
@@ -1,10 +1,11 @@
 import type { DomUtils } from '@qualweb/util';
 import type { QWPage } from '@qualweb/qw-page';
 import { ModuleTranslator } from '@qualweb/core/locale';
-import { EvaluationModuleDefinition, ExecutableModuleContext, ModuleReport, ModuleType } from '@qualweb/core/evaluation';
+import { EvaluationModuleDefinition, ExecutableModuleContext, ModuleOptions, ModuleReport, ModuleType } from '@qualweb/core/evaluation';
 import { BestPracticesTester } from './lib/BestPracticesTester.object';
 import { QualwebPage } from '@qualweb/core/lib';
 import { BestPracticesModule } from './BestPracticesModule';
+import { TranslationOptions } from '@qualweb/locale';
 
 // TODO: these definitions should be pulled from the packages that define them.
 declare global {
@@ -15,11 +16,25 @@ declare global {
   }
 }
 
-export class BestPractices extends EvaluationModuleDefinition {
-  protected readonly type = ModuleType.BEST_PRACTICES;
-  protected readonly report = new ModuleReport(this.type);
-  protected readonly translator = new ModuleTranslator(this.type, this.translate);
-  protected readonly tester = new BestPracticesTester(this.report).init(this.translator);
+export class BestPractices extends EvaluationModuleDefinition<BestPracticesTester> {
+  protected readonly translator: ModuleTranslator;
+
+  public constructor(moduleOptions: ModuleOptions, translationOptions: TranslationOptions) {
+    const moduleType = ModuleType.BEST_PRACTICES;
+    const report = new ModuleReport(moduleType);
+    const tester = new BestPracticesTester(report);
+
+    super(
+      moduleType,
+      moduleOptions,
+      translationOptions,
+      report,
+      tester,
+    );
+
+    this.translator = new ModuleTranslator(this.type, this.translate);
+    this.tester.init(this.translator);
+  }
 
   getInstance(page: QualwebPage): ExecutableModuleContext {
     return new BestPracticesModule(page, {});
