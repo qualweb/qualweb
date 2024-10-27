c18cc4f362001c4fd20ddd53d8a83fad3b98a255
lindhart <johannes@the.homestead.dk>
2024-10-27 14:44:46 +0100

    test(qw-page): fix broken test case
    I've seen cases where trying to inject scripts into blank pages (or non-HTML pages) fails the test. For this particular test case, I've added an empty HTML file that is loaded prior to testing script injection.

--- packages/qw-page/test/page.spec.ts
+++ packages/qw-page/test/page.spec.ts
@@ -14,15 +14,22 @@ describe('QualWeb page', function () {
 
     const browser = await puppeteer.launch({ headless: true });
     // createIncognitoBrowserContext() is deprecated - is the incognito mode necessary?
-    const incognito = await browser.createBrowserContext();
-    const page = await incognito.newPage();
+    const browserContext = await browser.createBrowserContext();
+    const page = await browserContext.newPage();
 
-    await page.addScriptTag({ path: require.resolve('../dist/qw-page.bundle.js') });
+    await page.goto(`file://${__dirname}/fixtures/empty.html`);
+
+    try {
+      await page.addScriptTag({ path: require.resolve('../dist/qw-page.bundle.js') });
+    } catch (_error: unknown) {
+      const error = _error as Error;
+      expect.fail(error.message);
+    }
 
     expect(await page.evaluate(() => window.qwPage));
 
     await page.close();
-    await incognito.close();
+    await browserContext.close();
     await browser.close();
   });
 });
