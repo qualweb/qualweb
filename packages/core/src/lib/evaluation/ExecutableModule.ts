f232bf5efb21098efff53882d9a623824026b798
Johannes L. Borresen <jlb@inqludeit.dk>
2024-10-28 15:41:10 +0100

    docs(core): add a bit of commentary to `ExecutableModuleContext`

--- packages/core/src/lib/evaluation/ExecutableModule.ts
+++ packages/core/src/lib/evaluation/ExecutableModule.ts
@@ -9,10 +9,13 @@ import type { TranslationOptions } from '@qualweb/locale';
 import type { QualwebPage } from '..';
 
 /**
- * This class represents an *INSTANCE* of an evaluation module that is meant to
- * run tests on an actual page.
- * CONTRAST this with {@link EvaluationModuleDefinition}, which represents the
- * overall definition and configuration of a module for an entire batch of URLs.
+ * This class is responsible for executing an evaluation module on a Puppeteer
+ * page. Separate modules (ACT-Rules, WCAG-techniques, etc) extend this class
+ * with module-specific steps for evaluation.
+ * CONTRAST this with {@link EvaluationModuleDefinition}, which represents
+ * the overall definition of a module as well as its in-page execution. Where
+ * this class runs outside of Puppeteer, {@link EvaluationModuleDefinition}s run
+ * within a page's context.
  */
 export abstract class ExecutableModuleContext {
   public abstract readonly name: ModuleType;
@@ -32,7 +35,21 @@ export abstract class ExecutableModuleContext {
     return this.runModule(page, this.options, translate, data);
   }
 
+  /**
+   * Should return the name of the NPM/node package that contains this module's
+   * code. Used to determine the path to the JS bundle when injecting code into
+   * pages.
+   */
   protected abstract getModulePackage?(): string;
+
+  /**
+   * When called, should perform the actual evaluation portion of the module.
+   * @param page Page context that the module should run in.
+   * @param options Configuration for running the module. This includes stuff
+   * like include/exclude lists for rules.
+   * @param translate Localization settings.
+   * @param data Additional contextual information about the execution context.
+   */
   protected abstract runModule(
     page: QualwebPage,
     options: ModuleOptions,
