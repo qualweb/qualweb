3bd784be9e9d02a08c187f7f7b7b9e6f0ebf61a4
Johannes L. Borresen <jlb@inqludeit.dk>
2024-10-31 12:03:33 +0100

    fix(counter): do not import code that is independently injected into page context
    There's probably an elegant way to do this, but @ts-expect-error suppresses for now.

--- packages/counter/src/CounterModule.ts
+++ packages/counter/src/CounterModule.ts
@@ -1,7 +1,6 @@
 import type { CounterReport } from '@qualweb/core/evaluation';
 import { ExecutableModuleContext, ModuleType } from '@qualweb/core/evaluation';
 import type { QualwebPage } from '@qualweb/core/lib';
-import { executeCounter } from './executeCounter';
 
 export class CounterModule extends ExecutableModuleContext {
   public readonly name = ModuleType.COUNTER;
@@ -12,6 +11,7 @@ export class CounterModule extends ExecutableModuleContext {
 
   protected runModule(page: QualwebPage): Promise<CounterReport> {
     return page.evaluate(() => {
+      // @ts-expect-error executCounter should be defined in page's context.
       return executeCounter();
     });
   }
