726ad828b8ef662be7945c987368e8144c6fbe25
lindhart <johannes@the.homestead.dk>
2024-10-31 10:36:52 +0100

    refactor(counter): moved `executeCounter` into its own file and additionally export `CounterModule` as `Counter`

---
+++ packages/counter/src/executeCounter.ts
@@ -0,0 +1,35 @@
+import type { CounterReport } from '@qualweb/core/evaluation';
+
+function executeCounter(): CounterReport {
+  const report: CounterReport = {
+    type: 'counter',
+    data: {
+      roles: {},
+      tags: {}
+    }
+  };
+
+  const elementList = window.qwPage.getElements('*');
+
+  for (const element of elementList ?? []) {
+    const role = window.AccessibilityUtils.getElementRole(element);
+    const tag = element.getElementTagName();
+
+    if (role) {
+      if (report.data.roles[role] === undefined) {
+        report.data.roles[role] = 0;
+      }
+
+      report.data.roles[role]++;
+    }
+
+    if (report.data.tags[tag] === undefined) {
+      report.data.tags[tag] = 0;
+    }
+    report.data.tags[tag]++;
+  }
+
+  return report;
+}
+
+export { executeCounter };
