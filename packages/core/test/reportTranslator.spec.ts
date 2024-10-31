b3051fd69268cc4e141ecccb506d0e3a0e95b9d6
Johannes L. Borresen <jlb@inqludeit.dk>
2024-10-31 12:27:25 +0100

    test(core): improvements to translation tests
    - Isolate `report` so a fresh copy is loaded before each test. Not having this made it harder to catch an unintended in-place modification bug.
    - Check that translation function does not perform in-place modification of input report in most tests.
    - Update fallback test. There doesn't seem to be a fallback to 'en', but just an exception.

--- packages/core/test/reportTranslator.spec.ts
+++ packages/core/test/reportTranslator.spec.ts
@@ -2,28 +2,45 @@ import { expect } from 'chai';
 import type { QualwebReport } from '../src';
 import { ReportTranslator } from '../src';
 import { LocaleFetcher } from '@qualweb/locale';
-
-import reportJson from './fixtures/reportTranslator.report.json';
-
-const report = reportJson as unknown as QualwebReport;
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import clone from 'lodash.clonedeep';
 
 describe('Testing translation', function () {
+  let report: QualwebReport;
+
+  this.beforeEach(() => {
+    report = JSON.parse(readFileSync(path.resolve(__dirname, 'fixtures/reportTranslator.report.json'), 'utf-8'));
+  })
+
   it('Should translate to fi: using locales.fi', function () {
+    const originalReport = clone(report);
     const translatedReport = new ReportTranslator(LocaleFetcher.get('fi')).translate(report);
 
+    // Ensure ReportTranslator did not modify the input vaule.
+    expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.equal(originalReport.modules['act-rules']!.assertions['QW-ACT-R1'].name);
+
+    // Ensure ReportTranslator correctly translated the output value.
     expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.be.not.equal(
       translatedReport.modules['act-rules']!.assertions['QW-ACT-R1'].name
     );
   });
 
   it('Should translate to fi: using "fi"', function () {
+    const originalReport = clone(report);
     const translatedReport = new ReportTranslator('fi').translate(report);
+
+    // Ensure ReportTranslator did not modify the input vaule.
+    expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.equal(originalReport.modules['act-rules']!.assertions['QW-ACT-R1'].name);
+
     expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.be.not.equal(
       translatedReport.modules['act-rules']!.assertions['QW-ACT-R1'].name
     );
   });
 
   it('Should translate to fi: using ownLocalObject', function () {
+    const originalReport = clone(report);
+
     const fiLocale = {
       'act-rules': {
         'QW-ACT-R1': {
@@ -38,19 +55,23 @@ describe('Testing translation', function () {
 
     const translatedReport = new ReportTranslator(fiLocale).translate(report);
 
+    // Ensure ReportTranslator did not modify the input vaule.
+    expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.equal(originalReport.modules['act-rules']!.assertions['QW-ACT-R1'].name);
+
     expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.be.not.equal(
       translatedReport.modules['act-rules']!.assertions['QW-ACT-R1'].name
     );
   });
 
-  it('Should fallback to "en": using "es" which is not supported', function () {
-    const translatedReport = new ReportTranslator('es' as any).translate(report);
-    expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.be.equal(
-      translatedReport.modules['act-rules']!.assertions['QW-ACT-R1'].name
-    );
+  it('Should throw Error when using unsupported locale', function () {
+    expect(() => {
+      new ReportTranslator('es' as any).translate(report);
+    }).to.throw();
   });
 
   it('Name should fallback to "en": using ownLocaleObject with partial translation', function () {
+    const originalReport = clone(report);
+
     const ptLocale = {
       'act-rules': {
         'QW-ACT-R1': {
@@ -63,6 +84,10 @@ describe('Testing translation', function () {
     };
 
     const translatedReport = new ReportTranslator(ptLocale).translate(report);
+
+    // Ensure ReportTranslator did not modify the input vaule.
+    expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.equal(originalReport.modules['act-rules']!.assertions['QW-ACT-R1'].name);
+
     expect(report.modules['act-rules']!.assertions['QW-ACT-R1'].name).to.be.equal(
       translatedReport.modules['act-rules']!.assertions['QW-ACT-R1'].name
     );
