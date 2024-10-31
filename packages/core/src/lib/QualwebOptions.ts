d63b71879f1f2f5101b5e12cc4d0540dd48fe854
lindhart <johannes@the.homestead.dk>
2024-10-31 22:51:57 +0100

    docs(core): documentation pass on `QualwebOptions`

--- packages/core/src/lib/QualwebOptions.ts
+++ packages/core/src/lib/QualwebOptions.ts
@@ -6,21 +6,82 @@ import { PuppeteerLifeCycleEvent } from 'puppeteer';
 import { ExecutableModuleContext } from './evaluation';
 
 export type QualwebOptions = {
+  /**
+   * Single URL to evaluate.
+   */
   url?: string;
+
+  /**
+   * Multiple URLs to evaluate.
+   */
   urls?: string[];
+
+  /**
+   * Load URLs from file. One URL per line.
+   */
   file?: string;
+
+  /**
+   * Use @qualweb/crawler to crawl this URL and evaluate.
+   * @see {@link QualwebOptions.crawlOptions} for setting additional options.
+   */
   crawl?: string;
-  html?: string;
-  timeout?: number;
-  maxParallelEvaluations?: number;
-  log?: LogOptions;
-  waitUntil?: PuppeteerLifeCycleEvent | PuppeteerLifeCycleEvent[];
-  viewport?: PageOptions;
-  validator?: string;
-  report?: 'earl' | 'earl-a';
-  translate?: TranslationOptions;
+
+  /**
+   * Additional options passed to @qualweb/crawler when
+   * {@link QualwebOptions.crawl} is set.
+   */
   crawlOptions?: CrawlOptions;
+
+  /**
+   * Raw HTML to evaluate.
+   */
+  html?: string;
+
+  /**
+   * Timeout to reach remote URLs, in milliseconds.
+   */
+  timeout?: number;
+
+  // FIXME: appears to be unused in code.
+  maxParallelEvaluations?: number;
+
+  /**
+   * Enable/disable logging to file and stdout.
+   */
+  log?: LogOptions;
+
+  /**
+   * Override the default life cycle event that signals QualWeb to start
+   * evaluating. Defaults to 'load'.
+   */
+  waitUntil?: PuppeteerLifeCycleEvent | PuppeteerLifeCycleEvent[];
+
+  /**
+   * Override the viewport used by Puppeteer.
+   */
+  viewport?: PageOptions;
+
+  /**
+   * HTML validator service endpoint. The url will be attached after of the
+   * given endpoint.
+   * FIXME: this needs better documentation. Is this a standard sort of validator?
+   */
+  validator?: string;
+
+  // FIXME: doesn't seem to be in use by core?
+  report?: 'earl' | 'earl-a';
+
+  /**
+   * Translate report to the specified locale. See @qualweb/locale for a list
+   * of supported languages.
+   * Defaults to 'en'.
+   */
+  translate?: TranslationOptions;
+
+  // FIXME: appears to be unused in code.
   'save-name'?: string;
+  
   /**
    * Array of modules to run for each URL in the configuration. See the
    * individual module's readme for specific usage, but it generally boils down
