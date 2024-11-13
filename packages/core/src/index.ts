acccbe304535aa6438c89f979c70c7b2280a546f
lindhart <johannes@the.homestead.dk>
2024-11-14 00:07:11 +0100

    feat(core): re-export `PuppeteerLifeCycleEvent`
    Stuff like this is fairly useful to export to clients, so clients don't have to use a separate puppeteer dependency and risk version incompatibilities.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,5 @@ export * from './lib/evaluation';
 export * from './lib/i18n';
 
 // TODO: consider what else should be exposed to other callers.
-export { QualwebOptions } from './lib/QualwebOptions';
+export * from './lib/QualwebOptions';
 export { CounterReport } from './lib/evaluation/CounterReport';
