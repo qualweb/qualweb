105bdc2d76cf86cba3ce38d3d3a0728b9cd37722
Filipe Dávila <filipedavila6@gmail.com>
2025-06-04 19:15:19 +0100

    update: logic for receiving new options and saving from runner for evaluation. Also now Cui Checks support async calls inside them as each check is Promise.

--- packages/cui-checks/src/CuiChecksRunner.ts
+++ packages/cui-checks/src/CuiChecksRunner.ts
@@ -2,14 +2,17 @@ import { ModuleTranslator } from '@qualweb/core/locale';
 import { EvaluationModuleDefinition, ModuleOptions, ModuleReport, ModuleType } from '@qualweb/core/evaluation';
 import { CUIChecksTester } from './lib/CUIChecksTester.object';
 import { TranslationOptions } from '@qualweb/locale';
+import { setBrowserFilePath } from './lib/readability';
 import { QWCUI_Selectors } from './lib/selectors';
 
 export class CUIChecksRunner extends EvaluationModuleDefinition<CUIChecksTester> {
   protected readonly translator = new ModuleTranslator(this.type, this.translate);
 
-  public constructor(moduleOptions: ModuleOptions, translationOptions: TranslationOptions) {
+  public constructor(moduleOptions: ModuleOptions, translationOptions: TranslationOptions, filePath?: string) {
+    if (filePath) setBrowserFilePath(filePath);
     const moduleType = ModuleType.CUI_CHECKS;
     const report = new ModuleReport(moduleType);
+    const settings = Object.fromEntries(Object.entries(moduleOptions.settings || {}));
     const tester = new CUIChecksTester(report);
 
     super(
@@ -21,16 +24,31 @@ export class CUIChecksRunner extends EvaluationModuleDefinition<CUIChecksTester>
     );
 
     this.translator = new ModuleTranslator(this.type, this.translate);
+    this.tester.setSettings(settings);
     this.tester.init(this.translator);
+    
+   
     if (moduleOptions.selectors) {
     let selectors:QWCUI_Selectors ={
       QW_CC_WINDOW: moduleOptions.selectors.QW_CC_WINDOW,
       QW_CC_DIALOG: moduleOptions.selectors.QW_CC_DIALOG,
       QW_CC_MESSAGES: moduleOptions.selectors.QW_CC_MESSAGES,
       QW_CC_MIC: moduleOptions.selectors.QW_CC_MIC,
-      QW_CC_INPUT: moduleOptions.selectors.QW_CC_INPUT
-    }    
-    this.tester.configureSelectors(selectors);
+      QW_CC_INPUT: moduleOptions.selectors.QW_CC_INPUT,
+      QW_CURRENCY: moduleOptions.selectors.QW_CURRENCY,
+      QW_QUESTIONS: moduleOptions.selectors.QW_QUESTIONS,
+      QW_DATE: moduleOptions.selectors.QW_DATE,
+     
+    }
+  
+      this.tester.configureSelectors(selectors);
+
     }
   }
+
+  public async executeTests(): Promise<this> {
+    await (this.tester as CUIChecksTester).execute();
+    return this;
+  }
+  
 }
