8424084862476a8edbb530a65fad7c0a1f5ce513
Filipe Dávila <filipedavila6@gmail.com>
2025-06-04 19:32:16 +0100

    Update: Check object now has access to setings passed to runner from extension

--- packages/cui-checks/src/lib/CUIChecksTester.object.ts
+++ packages/cui-checks/src/lib/CUIChecksTester.object.ts
@@ -1,56 +1,57 @@
 import type { ModuleTranslator } from '@qualweb/core/locale';
 import { Tester } from '@qualweb/core/evaluation';
-import {generateMappings} from './mapping';
+import { generateMappings } from './mapping';
 import * as checks from '../checks';
 import { QWCUI_Selectors } from './selectors';
-
+import { Check } from './Check.object';
 
 export class CUIChecksTester extends Tester {
-
   private mapping: { [key: string]: string[] } = {};
+  private settings: { [key: string]: string | number | boolean } = {};
   public init(translator: ModuleTranslator): this {
     for (const check in checks) {
-      const checkObject = new checks[check as keyof typeof checks](translator);
+      const checkObject = new checks[check as keyof typeof checks](translator, this.settings);
       this.assertions.set(checkObject.getCode(), checkObject);
       this.toExecute[checkObject.getCode()] = true;
     }
     return this;
   }
 
-  public execute(): void {
-    this.executeChecks();
+  public async execute(): Promise<void> {
+    await this.executeChecks();
   }
 
   public configureSelectors(uiSelectors: QWCUI_Selectors): void {
     this.mapping = generateMappings(uiSelectors);
-    console.log("Generated mapping",this.mapping);
   }
-  
+  public setSettings(settings: { [key: string]: string | number | boolean }): void {
+    this.settings = settings;
+  }
 
-
-  private executeChecks(): void {
+  private async executeChecks(): Promise<void> {
     const selectors = Object.keys(this.mapping);
     for (const selector of selectors ?? []) {
       for (const check of this.mapping[selector as keyof typeof this.mapping] ?? []) {
         if (this.toExecute[check]) {
-          this.executeCheck(check, selector);
+          await this.executeCheck(check, selector);
         }
       }
     }
   }
 
-  private executeCheck(check: string, selector: string): void {
+  private async executeCheck(check: string, selector: string): Promise<void> {
     const checkObject = this.assertions.get(check);
     if (checkObject) {
       const elements = window.qwPage.getElements(selector);
       if (elements.length > 0) {
-        elements.forEach((element) => checkObject.execute?.(element));
+        for (const element of elements) {
+          await (checkObject as Check).execute?.(element);
+        }
       } else {
-        checkObject.execute?.();
+        await (checkObject as Check).execute?.();
       }
 
       this.report.addAssertionResult(checkObject);
     }
   }
-
 }
