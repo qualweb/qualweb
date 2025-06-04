870987330d6f3701a64e448ff3890604513c2e58
Filipe Dávila <filipedavila6@gmail.com>
2025-06-04 19:32:54 +0100

    Update: list of selectors expected from extension

--- packages/cui-checks/src/lib/selectors.ts
+++ packages/cui-checks/src/lib/selectors.ts
@@ -1,7 +1,10 @@
-export interface   QWCUI_Selectors {
-    QW_CC_WINDOW: string;
-    QW_CC_DIALOG: string;
-    QW_CC_MESSAGES: string;
-    QW_CC_MIC: string;
-    QW_CC_INPUT: string;
+export interface QWCUI_Selectors {
+  QW_CC_WINDOW: string;
+  QW_CC_DIALOG: string;
+  QW_CC_MESSAGES: string;
+  QW_CC_MIC: string;
+  QW_CC_INPUT: string;
+  QW_QUESTIONS?: string;
+  QW_CURRENCY?: string;
+  QW_DATE?: string;
 }
