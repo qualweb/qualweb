f897ac26864ab623909afc198045efd16f3cbd8a
Filipe Dávila <filipedavila6@gmail.com>
2025-06-07 18:49:30 +0100

    Update: Mapping for selectors for check from 1 to 6

--- packages/cui-checks/src/lib/mapping.ts
+++ packages/cui-checks/src/lib/mapping.ts
@@ -1,18 +1,30 @@
 import { QWCUI_Selectors } from './selectors';
 
-let mapping: { [key: string]: string[] } = {};
+let mapping: { [key: string]: string[] } = {
+  QW_CC_MIC: ['QW-CUI-C1'],
+  QW_CC_MESSAGES: ['QW-CUI-C2', 'QW-CUI-C4'],
+  QW_QUESTIONS: ['QW-CUI-C5'],
+  QW_CURRENCY: ['QW-CUI-C6'],
+  QW_DATE: ['QW-CUI-C3']
+};
 
-function   generateMappings(uiSelectors: QWCUI_Selectors): { [key: string]: string[] } {
+function generateMappings(uiSelectors: QWCUI_Selectors): { [key: string]: string[] } {
   if (!uiSelectors) {
     return {};
   }
 
-  return Object.entries(mapping).reduce((acc, [key, value]) => {
-    acc[uiSelectors.QW_CC_WINDOW + ' > ' + key] = value;
-    return acc;
-  }, {
-    [uiSelectors.QW_CC_MIC]: ['QW-CUI-C1']
-  });
-};
+  return Object.entries(mapping).reduce(
+    (acc, [key, value]) => {
+      if (key in uiSelectors) {
+        acc[uiSelectors.QW_CC_WINDOW + ' ' + uiSelectors[key as keyof QWCUI_Selectors]] = value;
+      } else {
+        acc[uiSelectors.QW_CC_WINDOW + ' ' + key] = value;
+      }
+
+      return acc;
+    },
+    {} as { [key: string]: string[] }
+  );
+}
 
 export { generateMappings, mapping };
