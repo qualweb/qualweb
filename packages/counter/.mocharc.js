7289284004d46adcf425ee176dc1a0ee0da3a395
Filipe Dávila <filipedavila6@gmail.com>
2026-03-27 09:22:26 +0000

    fix(counter): enhance executeCounter logic to handle empty element lists and improve tag counting

--- packages/counter/.mocharc.js
+++ packages/counter/.mocharc.js
@@ -1,3 +1,8 @@
 const baseOptions = require('../../.mocharc.base.js');
 
-module.exports = baseOptions;
+module.exports = {
+    ...baseOptions,
+  require: 'ts-node/register', 
+  extension: ['ts'],
+  spec: ['test/**/*.spec.ts'],
+};
